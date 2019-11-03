cfa36aacae9b57b78058372b514d5ee3c98a2573
lzhlyle <lzhlyle@msn.cn>
2019-11-03 12:15:01 +0800

    Add solution in recite and self: 200. number-of-islands

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no200/NumberOfIslands.java
@@ -0,0 +1,47 @@
+package com.lzhlyle.leetcode.recite.no200;
+
+public class NumberOfIslands {
+    public int numIslands(char[][] grid) {
+        // base condition
+        if (grid.length <= 0 || grid[0].length <= 0) return 0;
+
+        // dfs, flood fill
+        int count = 0;
+        for (int row = 0; row < grid.length; row++) {
+            for (int column = 0; column < grid[row].length; column++) {
+                if (grid[row][column] == '1') {
+                    count++;
+                    this._sinkDown(row, column, grid);
+                }
+            }
+        }
+        return count;
+    }
+
+    private void _sinkDown(int row, int column, char[][] grid) {
+        // terminator
+        if (grid[row][column] == '0') return;
+
+        // process
+        grid[row][column] = '0'; // sink down
+
+        // drill down
+        if (column + 1 < grid[row].length) this._sinkDown(row, column + 1, grid); // east
+        if (column - 1 >= 0) this._sinkDown(row, column - 1, grid); // west
+        if (row + 1 < grid.length) this._sinkDown(row + 1, column, grid); // south
+        if (row - 1 >= 0) this._sinkDown(row - 1, column, grid); // north
+
+        // reverse state
+    }
+
+    public static void main(String[] args) {
+        char[][] grid = new char[][]{
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
+        int res = new NumberOfIslands().numIslands(grid);
+        System.out.println(res);
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/self/no200/NumberOfIslands.java
@@ -0,0 +1,50 @@
+package com.lzhlyle.leetcode.self.no200;
+
+public class NumberOfIslands {
+    public int numIslands(char[][] grid) {
+        // base condition
+
+        int count = 0;
+
+        // traversal
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[row].length; col++) {
+                if (grid[row][col] == '1') {
+                    count++;
+                    // dfs
+                    this._sinkDown(grid, row, col);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private void _sinkDown(char[][] grid, int row, int col) {
+        // terminator
+        if (grid[row][col] == '0') return;
+
+        // process
+        grid[row][col] = '0';
+
+        // drill down
+        if (col + 1 < grid[row].length) this._sinkDown(grid, row, col + 1); // east
+        if (col - 1 >= 0) this._sinkDown(grid, row, col - 1); // west
+        if (row + 1 < grid.length) this._sinkDown(grid, row + 1, col); // south
+        if (row - 1 >= 0) this._sinkDown(grid, row - 1, col); // north
+
+        // reverse state
+    }
+
+
+    public static void main(String[] args) {
+        char[][] grid = new char[][]{
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
+        int res = new NumberOfIslands().numIslands(grid);
+        System.out.println(res);
+    }
+}
