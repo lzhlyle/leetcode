0cfab7fdfb92c60d1f0a4b04d7cd3f0e0728e039
lzhlyle <lzhlyle@msn.cn>
2020-02-05 08:53:30 +0800

    recite and self: 206.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no206/ReverseList.java
@@ -0,0 +1,11 @@
+package com.lzhlyle.leetcode.recite.no206;
+
+public class ReverseList {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode res = reverseList(head.next); // 5 -> 4 -> 3|1 -> 2
+        head.next.next = head; // 5 -> 4 -> 3 -> 2 <-> 1
+        head.next = null; // 5 -> 4 -> 3 -> 2 -> 1 -> null
+        return res;
+    }
+}
