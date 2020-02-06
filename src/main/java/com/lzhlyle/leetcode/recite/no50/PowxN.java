1644685a594c3f60329baed091f8814a6db2adeb
lzhlyle <lzhlyle@msn.cn>
2020-02-06 15:58:36 +0800

    recite and self: 50.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no50/PowxN.java
@@ -0,0 +1,12 @@
+package com.lzhlyle.leetcode.recite.no50;
+
+public class PowxN {
+    public double myPow(double x, int n) {
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        if (n == -1) return 1 / x;
+        double half = myPow(x, n / 2);
+        double rest = myPow(x, n % 2);
+        return rest * half * half;
+    }
+}
