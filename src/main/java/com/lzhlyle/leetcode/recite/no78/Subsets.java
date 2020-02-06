f2f68bac900444e152c7e2316da450392facbba5
lzhlyle <lzhlyle@msn.cn>
2020-02-06 16:22:27 +0800

    recite and self: 78.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no78/Subsets.java
@@ -0,0 +1,19 @@
+package com.lzhlyle.leetcode.recite.no78;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        int len = nums.length;
+        List<List<Integer>> res = new LinkedList<>();
+        for (int i = 0; i < (1 << len); i++) {
+            List<Integer> adding = new LinkedList<>();
+            for (int j = 0; j < len; j++) {
+                if (((i >> j) & 1) == 1) adding.add(nums[j]);
+            }
+            res.add(adding);
+        }
+        return res;
+    }
+}
