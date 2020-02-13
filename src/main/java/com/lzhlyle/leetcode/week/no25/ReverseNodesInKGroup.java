49e4e1394281f75226ceecf7da57c9a16c0c459a
lzhlyle <lzhlyle@msn.cn>
2020-02-13 17:06:26 +0800

    week: 25.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no25/ReverseNodesInKGroup.java
@@ -0,0 +1,30 @@
+package com.lzhlyle.leetcode.week.no25;
+
+public class ReverseNodesInKGroup {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null) return head;
+        // look up end node
+        ListNode end = head;
+        for (int i = 0; i < k; i++) {
+            if (end == null) return head;
+            end = end.next;
+        }
+        // reverse
+        ListNode res = _reverse(head, end);
+        head.next = reverseKGroup(end, k);
+        return res;
+    }
+
+    private ListNode _reverse(ListNode begin, ListNode end) {
+        ListNode prev = null, curr = begin, next;
+        while (curr != end) {
+            next = curr.next;
+            // change
+            curr.next = prev;
+            // push
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
