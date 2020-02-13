860f7fe8003ffbecc5178e8e4c7d081da5277591
lzhlyle <lzhlyle@msn.cn>
2020-02-13 14:31:58 +0800

    week: 98.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no98/TreeNode.java
@@ -0,0 +1,10 @@
+package com.lzhlyle.leetcode.week.no98;
+
+public class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/week/no98/ValidateBinarySearchTree_Recursion.java
@@ -0,0 +1,15 @@
+package com.lzhlyle.leetcode.week.no98;
+
+public class ValidateBinarySearchTree_Recursion {
+    public boolean isValidBST(TreeNode root) {
+        return _valid(root, null, null);
+    }
+
+    private boolean _valid(TreeNode node, Integer min, Integer max) {
+        if (node == null) return true;
+        int val = node.val;
+        if (min != null && val <= min) return false;
+        if (max != null && val >= max) return false;
+        return _valid(node.left, min, val) && _valid(node.right, val, max);
+    }
+}
