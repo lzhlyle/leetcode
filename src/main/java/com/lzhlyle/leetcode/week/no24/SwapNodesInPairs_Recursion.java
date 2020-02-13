69eb212a91781b0071e5290d18909af65e4455be
lzhlyle <lzhlyle@msn.cn>
2020-02-13 16:58:31 +0800

    week: 24.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no24/SwapNodesInPairs_Recursion.java
@@ -0,0 +1,11 @@
+package com.lzhlyle.leetcode.week.no24;
+
+public class SwapNodesInPairs_Recursion {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode n = head.next;
+        head.next = swapPairs(n.next);
+        n.next = head;
+        return n;
+    }
+}
