91602688a63940d1c6d04613dfec02c3a58e216f
lzhlyle <lzhlyle@msn.cn>
2020-02-16 23:19:16 +0800

    tomorrow: 1143.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no1143/LongestCommonSubsequence_DP_Adv.java
@@ -0,0 +1,22 @@
+package com.lzhlyle.leetcode.tomorrow.no1143;
+
+public class LongestCommonSubsequence_DP_Adv {
+    public int longestCommonSubsequence(String text1, String text2) {
+        char[] arrI = text1.toCharArray(), arrJ = text2.toCharArray();
+        int lenI = arrI.length, lenJ = arrJ.length;
+        if (lenI == 0 || lenJ == 0) return 0;
+
+        int[] dp = new int[lenJ + 1]; // default column: empty sub-sequence ""
+        int northwest;
+        for (int i = 1; i <= lenI; i++) {
+            northwest = 0;
+            for (int j = 1; j <= lenJ; j++) {
+                int nextNorthwest = dp[j];
+                if (arrI[i - 1] == arrJ[j - 1]) dp[j] = northwest + 1;
+                else dp[j] = Math.max(dp[j], dp[j - 1]);
+                northwest = nextNorthwest;
+            }
+        }
+        return dp[lenJ];
+    }
+}
