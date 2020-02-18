14b128f2b1f23462599e8cd2e4a83d40b0a4cfd9
lzhlyle <lzhlyle@msn.cn>
2020-02-18 21:23:23 +0800

    recite and self: 714.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no714/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -0,0 +1,15 @@
+package com.lzhlyle.leetcode.self.no714;
+
+public class BestTimeToBuyAndSellStockWithTransactionFee {
+    public int maxProfit(int[] prices, int fee) {
+        int len = prices.length;
+        if (len < 2) return 0;
+        int dp0 = 0, dp1 = -prices[0];
+        for (int i = 1; i < len; i++) {
+            int oriDp0 = dp0;
+            if (dp1 + prices[i] - fee > dp0) dp0 = dp1 + prices[i] - fee;
+            if (oriDp0 - prices[i] > dp1) dp1 = oriDp0 - prices[i];
+        }
+        return dp0;
+    }
+}
