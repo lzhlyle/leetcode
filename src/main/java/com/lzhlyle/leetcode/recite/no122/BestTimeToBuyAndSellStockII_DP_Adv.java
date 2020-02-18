f87e341559ab36b05d8d0a262bee37dcf77e4fc4
lzhlyle <lzhlyle@msn.cn>
2020-02-18 19:04:17 +0800

    recite and self: 122.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no122/BestTimeToBuyAndSellStockII_DP_Adv.java
@@ -0,0 +1,15 @@
+package com.lzhlyle.leetcode.recite.no122;
+
+public class BestTimeToBuyAndSellStockII_DP_Adv {
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if (len < 2) return 0;
+        int dp0 = 0, dp1 = -prices[0]; // 0:empty, 1:hold
+        for (int i = 1; i < len; i++) {
+            int oriDp0 = dp0;
+            if (dp1 + prices[i] > dp0) dp0 = dp1 + prices[i];
+            if (oriDp0 - prices[i] > dp1) dp1 = oriDp0 - prices[i];
+        }
+        return dp0;
+    }
+}
--- src/main/java/com/lzhlyle/leetcode/self/no122/BestTimeToBuyAndSellStockII_Greedy.java
+++ src/main/java/com/lzhlyle/leetcode/self/no122/BestTimeToBuyAndSellStockII_Greedy.java
@@ -1,13 +1,11 @@
-package com.lzhlyle.leetcode.recite.no122;
+package com.lzhlyle.leetcode.self.no122;
 
 public class BestTimeToBuyAndSellStockII_Greedy {
     public int maxProfit(int[] prices) {
-        // base condition
-        if (prices == null || prices.length < 2) return 0;
-
-        // greedy
+        int len = prices.length;
+        if (len < 2) return 0;
         int max = 0;
-        for (int i = 1; i < prices.length; i++) {
+        for (int i = 1; i < len; i++) {
             if (prices[i] > prices[i - 1]) max += prices[i] - prices[i - 1];
         }
         return max;
