5e7a6d543482e84188d96380a38b52f28ff1492d
lzhlyle <lzhlyle@msn.cn>
2020-02-19 09:51:52 +0800

    tomorrow: 309.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no309/BestTimeToBuyAndSellCtockWithCooldown_DP.java
@@ -0,0 +1,16 @@
+package com.lzhlyle.leetcode.tomorrow.no309;
+
+public class BestTimeToBuyAndSellCtockWithCooldown_DP {
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if (len < 2) return 0;
+        int dp0 = 0, dp1 = -prices[0], dpPre0 = dp0; // pre day
+        for (int i = 1; i < len; i++) {
+            int nextDbPre0 = dp0;
+            if (dp1 + prices[i] > dp0) dp0 = dp1 + prices[i];
+            if (dpPre0 - prices[i] > dp1) dp1 = dpPre0 - prices[i];
+            dpPre0 = nextDbPre0;
+        }
+        return dp0;
+    }
+}
