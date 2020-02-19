b040e0f9f168cbed602ba3dd9e3ced4f9f9a018a
lzhlyle <lzhlyle@msn.cn>
2020-02-19 20:39:52 +0800

    recite and self: 876.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no876/MiddleOfTheLinkedList.java
@@ -0,0 +1,12 @@
+package com.lzhlyle.leetcode.self.no876;
+
+public class MiddleOfTheLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
