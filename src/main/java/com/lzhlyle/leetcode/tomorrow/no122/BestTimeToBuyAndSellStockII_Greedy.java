0d6985d58a8161664e036eeb12c08d84fa6765c0
lzhlyle <lzhlyle@msn.cn>
2020-02-19 09:47:37 +0800

    tomorrow: 122.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no122/BestTimeToBuyAndSellStockII_Greedy.java
@@ -0,0 +1,13 @@
+package com.lzhlyle.leetcode.tomorrow.no122;
+
+public class BestTimeToBuyAndSellStockII_Greedy {
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if (len < 2) return 0;
+        int max = 0;
+        for (int i = 1; i < len; i++) {
+            if (prices[i] > prices[i - 1]) max += prices[i] - prices[i - 1];
+        }
+        return max;
+    }
+}
