00b5ee0317e7a53379c8c754436a85d74747b03b
lzhlyle <lzhlyle@msn.cn>
2020-02-19 19:33:32 +0800

    recite and self: 21.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no21/ListNode.java
@@ -0,0 +1,11 @@
+package com.lzhlyle.leetcode.recite.no21;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
---
+++ src/main/java/com/lzhlyle/leetcode/recite/no21/MergeTwoSortedLists_Recursion.java
@@ -0,0 +1,18 @@
+package com.lzhlyle.leetcode.recite.no21;
+
+public class MergeTwoSortedLists_Recursion {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        ListNode res;
+        if (l1.val < l2.val) {
+            res = l1;
+            l1 = l1.next;
+        } else {
+            res = l2;
+            l2 = l2.next;
+        }
+        res.next = mergeTwoLists(l1, l2);
+        return res;
+    }
+}
