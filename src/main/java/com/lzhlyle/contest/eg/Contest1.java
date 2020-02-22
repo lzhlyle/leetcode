47b80759b30c6199dd1c8f5b44a2552813bc5264
lzhlyle <lzhlyle@msn.cn>
2020-02-22 22:06:17 +0800

    Add contest example.

---
+++ src/main/java/com/lzhlyle/contest/eg/Contest1.java
@@ -0,0 +1,11 @@
+package com.lzhlyle.contest.eg;
+
+public class Contest1 {
+
+    public static void main(String[] args) {
+        Contest1 contest = new Contest1();
+        {
+            
+        }
+    }
+}
---
+++ src/main/java/com/lzhlyle/contest/eg/Contest2.java
@@ -0,0 +1,11 @@
+package com.lzhlyle.contest.eg;
+
+public class Contest2 {
+
+    public static void main(String[] args) {
+        Contest2 contest = new Contest2();
+        {
+            
+        }
+    }
+}
