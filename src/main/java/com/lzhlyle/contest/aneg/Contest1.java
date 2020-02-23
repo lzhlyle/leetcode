f7187e201485626cde4d82b61a086340d9837cfc
lzhlyle <lzhlyle@msn.cn>
2020-02-23 10:23:13 +0800

    rename eg package.

--- src/main/java/com/lzhlyle/contest/aneg/Contest1.java
+++ src/main/java/com/lzhlyle/contest/aneg/Contest1.java
@@ -3,6 +3,7 @@ package com.lzhlyle.contest.eg;
 public class Contest1 {
 
     public static void main(String[] args) {
+        System.out.println("begin");
         Contest1 contest = new Contest1();
         {
             
