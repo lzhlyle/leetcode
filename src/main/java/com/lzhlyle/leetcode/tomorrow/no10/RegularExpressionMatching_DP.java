c1b79fc651fcdd4794b95492eac1446b50965b07
lzhlyle <lzhlyle@msn.cn>
2020-02-23 09:54:05 +0800

    tomorrow: 10.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no10/RegularExpressionMatching_DP.java
@@ -0,0 +1,19 @@
+package com.lzhlyle.leetcode.tomorrow.no10;
+
+public class RegularExpressionMatching_DP {
+    public boolean isMatch(String s, String p) {
+        char[] sarr = s.toCharArray(), parr = p.toCharArray();
+        int slen = sarr.length, plen = parr.length;
+        boolean[][] dp = new boolean[slen + 1][plen + 1]; // +""
+        dp[slen][plen] = true;
+        for (int si = slen; si > -1; si--) {
+            for (int pi = plen - 1; pi > -1; pi--) {
+                boolean first = (si < slen) && (parr[pi] == '.' || parr[pi] == sarr[si]);
+                if (pi + 1 < plen && parr[pi + 1] == '*') {
+                    dp[si][pi] = dp[si][pi + 2] || (first && dp[si + 1][pi]);
+                } else dp[si][pi] = first && dp[si + 1][pi + 1];
+            }
+        }
+        return dp[0][0];
+    }
+}
