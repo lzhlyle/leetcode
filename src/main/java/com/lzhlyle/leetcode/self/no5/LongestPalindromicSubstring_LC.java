4ec7a3597581b4c45873c48f1dee7dc672273235
lzhlyle <lzhlyle@msn.cn>
2020-02-25 00:28:52 +0800

    refactor solution 5.

--- src/main/java/com/lzhlyle/leetcode/self/no5/LongestPalindromicSubstring_LC.java
+++ src/main/java/com/lzhlyle/leetcode/self/no5/LongestPalindromicSubstring_LC.java
@@ -5,23 +5,21 @@ public class LongestPalindromicSubstring_LC {
         char[] arr = s.toCharArray();
         int len = arr.length, begin = 0, maxLen = 1;
         if (len < 2) return s;
-        for (int l = 0; l < len; l++) {
-            // skip
-            int r = l--;
+        for (int i = 0; i < len; i++) {
+            int l = i - 1, r = i;
+            // skip the same
             while (r + 1 < len && arr[r] == arr[r + 1]) r++;
-            int nextL = r++;
+            i = r++;
             // expand
             while (l >= 0 && r < len && arr[l] == arr[r]) {
                 l--;
                 r++;
             }
-            // push
+            // record
             if (r - l - 1 > maxLen) {
                 maxLen = r - l - 1;
                 begin = l + 1;
             }
-            // reverse
-            l = nextL;
         }
         return s.substring(begin, begin + maxLen);
     }
