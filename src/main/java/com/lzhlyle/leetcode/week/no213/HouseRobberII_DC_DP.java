2bc19a3448ef7f80e6e2989fcb7730c2dc14aabc
lzhlyle <lzhlyle@msn.cn>
2020-02-25 12:18:51 +0800

    week: 213.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no213/HouseRobberII_DC_DP.java
@@ -0,0 +1,20 @@
+package com.lzhlyle.leetcode.week.no213;
+
+public class HouseRobberII_DC_DP {
+    public int rob(int[] nums) {
+        int len = nums.length;
+        if (len == 0) return 0;
+        if (len == 1) return nums[0];
+        return Math.max(_rob(nums, 0, len - 1), _rob(nums, 1, len));
+    }
+
+    private int _rob(int[] nums, int begin, int end) {
+        int dp0 = 0, dp1 = nums[begin];
+        for (int i = begin + 1; i < end; i++) {
+            int oriDp0 = dp0;
+            if (dp1 > dp0) dp0 = dp1;
+            dp1 = oriDp0 + nums[i];
+        }
+        return Math.max(dp0, dp1);
+    }
+}
