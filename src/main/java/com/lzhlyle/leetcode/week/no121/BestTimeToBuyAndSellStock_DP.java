46d11758aca935c1ea4a03cf6b3fd147280dc6bf
lzhlyle <lzhlyle@msn.cn>
2020-02-26 14:56:38 +0800

    week: 121.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no121/BestTimeToBuyAndSellStock_DP.java
@@ -0,0 +1,15 @@
+package com.lzhlyle.leetcode.week.no121;
+
+public class BestTimeToBuyAndSellStock_DP {
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if (len < 2) return 0;
+        int[][] dp = new int[len][2]; // 0:empty, 1:hold
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+        }
+        return dp[len - 1][0];
+    }
+}
