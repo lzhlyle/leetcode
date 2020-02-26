7c9458340c92dd10b3f8847705c12081ac3a1b64
lzhlyle <lzhlyle@msn.cn>
2020-02-26 15:25:56 +0800

    week: 123.

--- src/main/java/com/lzhlyle/leetcode/tomorrow/no123/BestTimeToBuyAndSellStockIII_DP_Adv.java
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no123/BestTimeToBuyAndSellStockIII_DP_Adv.java
@@ -4,14 +4,14 @@ public class BestTimeToBuyAndSellStockIII_DP_Adv {
     public int maxProfit(int[] prices) {
         int len = prices.length;
         if (len < 2) return 0;
-        int dp00, dp10, dp20, dp01, dp11, dp21;
+        int dp00, dp10, dp11, dp20, dp21;
         dp00 = dp10 = dp20 = 0;
-        dp01 = dp11 = dp21 = -prices[0];
+        dp11 = dp21 = -prices[0];
         for (int i = 1; i < len; i++) {
-            dp20 = Math.max(dp20, dp21 + prices[i]);
-            dp21 = Math.max(dp21, dp10 - prices[i]);
-            dp10 = Math.max(dp10, dp11 + prices[i]);
-            dp11 = Math.max(dp11, dp00 - prices[i]);
+            if (dp21 + prices[i] > dp20) dp20 = dp21 + prices[i];
+            if (dp10 - prices[i] > dp21) dp21 = dp10 - prices[i];
+            if (dp11 + prices[i] > dp10) dp10 = dp11 + prices[i];
+            if (dp00 - prices[i] > dp11) dp11 = dp00 - prices[i];
         }
         return dp20;
     }
