b6790299e702f751499f40de9300c9c826043bf8
lzhlyle <lzhlyle@msn.cn>
2020-02-27 18:17:52 +0800

    recite and self: 709.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no709/ToLowerCase.java
@@ -0,0 +1,11 @@
+package com.lzhlyle.leetcode.self.no709;
+
+public class ToLowerCase {
+    public String toLowerCase(String str) {
+        char[] arr = str.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 'A' && arr[i] <= 'Z') arr[i] += 'a' - 'A';
+        }
+        return String.valueOf(arr);
+    }
+}
