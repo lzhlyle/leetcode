40dbf4df30a2328f02ae2452b360a7e947c4b7d2
lzhlyle <lzhlyle@msn.cn>
2020-02-27 18:01:57 +0800

    recite and self: 8.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no8/StringToIntegerAtoi.java
@@ -0,0 +1,28 @@
+package com.lzhlyle.leetcode.self.no8;
+
+public class StringToIntegerAtoi {
+    public int myAtoi(String str) {
+        int len = str.length(), i = 0, sign = 1, total = 0;
+        if (len < 1) return 0;
+        while (i < len && str.charAt(i) == ' ') i++;
+
+        if (i < len && (str.charAt(i) == '+' || str.charAt(i) == '-')) {
+            if (str.charAt(i++) == '-') sign = -1;
+        }
+
+        while (i < len) {
+            int digit = str.charAt(i) - '0';
+            if (digit < 0 || digit > 9) break;
+
+            if (Integer.MAX_VALUE / 10 < total ||
+                    (Integer.MAX_VALUE / 10 == total && Integer.MAX_VALUE % 10 < digit)) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            total = 10 * total + digit;
+            i++;
+        }
+
+        return total * sign;
+    }
+}
