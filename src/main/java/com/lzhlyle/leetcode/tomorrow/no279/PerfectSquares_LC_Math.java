ace9607b263ac90a725f182f747624ae4a4912ca
lzhlyle <lzhlyle@msn.cn>
2020-02-29 17:57:00 +0800

    tomorrow: 279.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no279/PerfectSquares_LC_Math.java
@@ -0,0 +1,19 @@
+package com.lzhlyle.leetcode.tomorrow.no279;
+
+public class PerfectSquares_LC_Math {
+    public int numSquares(int n) {
+        while (n % 4 == 0) n >>= 2;
+        if (n % 8 == 7) return 4;
+
+        for (int i = 1; i * i <= n; i++) {
+            if (i * i == n) return 1;
+        }
+
+        for (int i = 1; i * i <= n; i++) {
+            int b = (int) Math.sqrt(n - i * i);
+            if (b * b + i * i == n) return 2;
+        }
+
+        return 3;
+    }
+}
