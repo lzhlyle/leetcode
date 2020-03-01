2c89ee9dae0622c9306ebb4da6e74300593da219
lzhlyle <lzhlyle@msn.cn>
2020-03-01 12:25:22 +0800

    recite and self: 225.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no225/MyStack.java
@@ -0,0 +1,47 @@
+package com.lzhlyle.leetcode.self.no225;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyStack {
+    private Queue<Integer> queue;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+
+    /**
+     * Push element x onto stack.
+     */
+    public void push(int x) {
+        int size = queue.size();
+        queue.add(x);
+        while (size-- > 0) {
+            queue.add(queue.remove());
+        }
+    }
+
+    /**
+     * Removes the element on top of the stack and returns that element.
+     */
+    public int pop() {
+        return queue.remove();
+    }
+
+    /**
+     * Get the top element.
+     */
+    public int top() {
+        return queue.element();
+    }
+
+    /**
+     * Returns whether the stack is empty.
+     */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
