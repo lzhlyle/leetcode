49d1a63e65f4fb079c86e47c4d357e7caad4701f
lzhlyle <lzhlyle@msn.cn>
2020-03-04 21:16:34 +0800

    tomorrow: 186.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no186/ReverseWordsInAStringIi.java
@@ -0,0 +1,21 @@
+package com.lzhlyle.leetcode.tomorrow.no186;
+
+public class ReverseWordsInAStringIi {
+    public void reverseWords(char[] s) {
+        reverse(s, 0, s.length - 1);
+        for (int r = 0, len = s.length, l = 0; r < len; r++) {
+            if (s[r] == ' ') {
+                reverse(s, l, r - 1);
+                l = r + 1;
+            } else if (r == len - 1) reverse(s, l, r);
+        }
+    }
+
+    private void reverse(char[] arr, int l, int r) {
+        while (l < r) {
+            arr[l] ^= arr[r];
+            arr[r] ^= arr[l];
+            arr[l++] ^= arr[r--];
+        }
+    }
+}
