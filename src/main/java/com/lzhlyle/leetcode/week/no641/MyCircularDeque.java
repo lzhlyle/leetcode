c501c0976b16286e88a706b9a37254785734dd8e
lzhlyle <lzhlyle@msn.cn>
2020-03-05 13:52:25 +0800

    week: 641.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no641/MyCircularDeque.java
@@ -0,0 +1,82 @@
+package com.lzhlyle.leetcode.week.no641;
+
+public class MyCircularDeque {
+    private int[] data;
+    private int front, rear;
+
+    /**
+     * Initialize your data structure here. Set the size of the deque to be k.
+     */
+    public MyCircularDeque(int k) {
+        data = new int[k + 1];
+        front = rear = 0;
+    }
+
+    /**
+     * Adds an item at the front of Deque. Return true if the operation is successful.
+     */
+    public boolean insertFront(int value) {
+        if (isFull()) return false;
+        front = (front - 1 + data.length) % data.length;
+        data[front] = value;
+        return true;
+    }
+
+    /**
+     * Adds an item at the rear of Deque. Return true if the operation is successful.
+     */
+    public boolean insertLast(int value) {
+        if (isFull()) return false;
+        data[rear] = value;
+        rear = (rear + 1) % data.length;
+        return true;
+    }
+
+    /**
+     * Deletes an item from the front of Deque. Return true if the operation is successful.
+     */
+    public boolean deleteFront() {
+        if (isEmpty()) return false;
+        front = (front + 1) % data.length;
+        return true;
+    }
+
+    /**
+     * Deletes an item from the rear of Deque. Return true if the operation is successful.
+     */
+    public boolean deleteLast() {
+        if (isEmpty()) return false;
+        rear = (rear - 1 + data.length) % data.length;
+        return true;
+    }
+
+    /**
+     * Get the front item from the deque.
+     */
+    public int getFront() {
+        if (isEmpty()) return -1;
+        return data[front];
+    }
+
+    /**
+     * Get the last item from the deque.
+     */
+    public int getRear() {
+        if (isEmpty()) return -1;
+        return data[(rear - 1 + data.length) % data.length];
+    }
+
+    /**
+     * Checks whether the circular deque is empty or not.
+     */
+    public boolean isEmpty() {
+        return front == rear;
+    }
+
+    /**
+     * Checks whether the circular deque is full or not.
+     */
+    public boolean isFull() {
+        return front == (rear + 1) % data.length;
+    }
+}
