3345dfa96ca8449031c30dc6cee6f4baeeb00aa4
lzhlyle <lzhlyle@msn.cn>
2020-03-06 20:31:10 +0800

    tomorrow: 1103.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no1103/DistributeCandiesToPeople.java
@@ -0,0 +1,15 @@
+package com.lzhlyle.leetcode.tomorrow.no1103;
+
+public class DistributeCandiesToPeople {
+    public int[] distributeCandies(int candies, int num_people) {
+        int[] res = new int[num_people];
+        int i = 0, next = 1;
+        while (candies > next) {
+            i %= num_people;
+            res[i++] += next;
+            candies -= next++;
+        }
+        res[i % num_people] += candies;
+        return res;
+    }
+}
