e4d25b841f5a0b382e8c96ed03d9c1180b6f8806
lzhlyle <lzhlyle@msn.cn>
2020-03-08 00:32:12 +0800

    recit and self: lcof-59-ii.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/lcof59ii/MaxQueue.java
@@ -0,0 +1,32 @@
+package com.lzhlyle.leetcode.recite.lcof59ii;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaxQueue {
+    Queue<Integer> data;
+    Deque<Integer> max; // desc
+
+    public MaxQueue() {
+        data = new LinkedList<>();
+        max = new LinkedList<>();
+    }
+
+    public int max_value() {
+        if (data.isEmpty()) return -1;
+        return max.getFirst();
+    }
+
+    public void push_back(int value) {
+        data.add(value);
+        while (!max.isEmpty() && max.getLast() < value) max.removeLast();
+        max.addLast(value);
+    }
+
+    public int pop_front() {
+        if (data.isEmpty()) return -1;
+        if (data.element().equals(max.getFirst())) max.removeFirst();
+        return data.remove();
+    }
+}
