f3cbe330d3e628f5ecd64f72a26c140ebc540a3e
lzhlyle <lzhlyle@msn.cn>
2020-03-11 00:51:25 +0800

    recite and self: 33.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no33/SearchInRotatedSortedArray_LC_Recite_Left.java
@@ -0,0 +1,19 @@
+package com.lzhlyle.leetcode.recite.no33;
+
+public class SearchInRotatedSortedArray_LC_Recite_Left {
+    public int search(int[] nums, int target) {
+        if (nums.length == 0) return -1;
+        int l = 0, r = nums.length - 1; // [l, r]
+        while (l < r) {
+            int mid = l + ((r - l) >> 1);
+            if (nums[l] < nums[mid]) { // [l, mid] asc
+                if (nums[l] <= target && target <= nums[mid]) r = mid;
+                else l = mid + 1;
+            } else { // [mid + 1, r] asc
+                if (nums[mid + 1] <= target && target <= nums[r]) l = mid + 1;
+                else r = mid;
+            }
+        }
+        return nums[l] == target ? l : -1;
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/self/no33/SearchInRotatedSortedArray_LC_Recite_Right.java
@@ -0,0 +1,19 @@
+package com.lzhlyle.leetcode.self.no33;
+
+public class SearchInRotatedSortedArray_LC_Recite_Right {
+    public int search(int[] nums, int target) {
+        if (nums.length == 0) return -1;
+        int l = 0, r = nums.length - 1; // [l, r]
+        while (l < r) {
+            int mid = l + ((r - l + 1) >>> 1);
+            if (nums[mid] < nums[r]) { // [mid, r] asc
+                if (nums[mid] <= target && target <= nums[r]) l = mid;
+                else r = mid - 1;
+            } else { // [l, mid - 1] asc
+                if (nums[l] <= target && target <= nums[mid - 1]) r = mid - 1;
+                else l = mid;
+            }
+        }
+        return nums[l] == target ? l : -1;
+    }
+}
