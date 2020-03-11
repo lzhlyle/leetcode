c77753ea67ec919a1124a41b926c772238471546
lzhlyle <lzhlyle@msn.cn>
2020-03-11 23:46:52 +0800

    tomorrow: 543.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no543/DiameterOfBinaryTree_Recursion.java
@@ -0,0 +1,17 @@
+package com.lzhlyle.leetcode.tomorrow.no543;
+
+public class DiameterOfBinaryTree_Recursion {
+    private int max = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return max;
+    }
+
+    private int depth(TreeNode node) {
+        if (node == null) return 0;
+        int l = depth(node.left), r = depth(node.right);
+        max = Math.max(max, l + r);
+        return Math.max(l, r) + 1;
+    }
+}
