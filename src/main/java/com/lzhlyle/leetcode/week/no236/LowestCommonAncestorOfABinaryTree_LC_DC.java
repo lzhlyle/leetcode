68e77f43f5475bc91d09864753fa9002e71911c2
lzhlyle <lzhlyle@msn.cn>
2020-03-14 15:55:48 +0800

    week: 236.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no236/LowestCommonAncestorOfABinaryTree_LC_DC.java
@@ -0,0 +1,11 @@
+package com.lzhlyle.leetcode.week.no236;
+
+public class LowestCommonAncestorOfABinaryTree_LC_DC {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode l = lowestCommonAncestor(root.left, p, q);
+        TreeNode r = lowestCommonAncestor(root.right, p, q);
+        if (l != null && r != null) return root;
+        return l == null ? r : l;
+    }
+}
