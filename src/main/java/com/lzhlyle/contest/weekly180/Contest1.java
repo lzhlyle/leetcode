36e7c744ceb218b65a4a199f3f8edca2a9acafd9
lzhlyle <lzhlyle@msn.cn>
2020-03-15 10:30:19 +0800

    tomorrow: 1071.

---
+++ src/main/java/com/lzhlyle/contest/weekly180/Contest1.java
@@ -0,0 +1,12 @@
+package com.lzhlyle.contest.weekly180;
+
+public class Contest1 {
+
+    public static void main(String[] args) {
+        System.out.println("begin");
+        Contest1 contest = new Contest1();
+        {
+            
+        }
+    }
+}
---
+++ src/main/java/com/lzhlyle/contest/weekly180/Contest3.java
@@ -0,0 +1,11 @@
+package com.lzhlyle.contest.weekly180;
+
+public class Contest3 {
+
+    public static void main(String[] args) {
+        Contest3 contest = new Contest3();
+        {
+            
+        }
+    }
+}
