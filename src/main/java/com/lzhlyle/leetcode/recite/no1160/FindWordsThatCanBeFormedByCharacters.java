97d3245e276669eabf6f910f805d98c671760e4a
lzhlyle <lzhlyle@msn.cn>
2020-03-17 00:19:47 +0800

    recite and self: 1160.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no1160/FindWordsThatCanBeFormedByCharacters.java
@@ -0,0 +1,22 @@
+package com.lzhlyle.leetcode.recite.no1160;
+
+public class FindWordsThatCanBeFormedByCharacters {
+    public int countCharacters(String[] words, String chars) {
+        char[] arr = new char[26];
+        for (char c : chars.toCharArray()) arr[c - 'a']++;
+        int res = 0;
+        for (String word : words) {
+            boolean learn = true;
+            char[] temp = new char[26];
+            for (char c : word.toCharArray()) {
+                temp[c - 'a']++;
+                if (temp[c - 'a'] > arr[c - 'a']) {
+                    learn = false;
+                    break;
+                }
+            }
+            if (learn) res += word.length();
+        }
+        return res;
+    }
+}
