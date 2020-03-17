7796f3a93e84a4ce2b397df25f9d7e030f44333c
lzhlyle <lzhlyle@msn.cn>
2020-03-17 19:17:27 +0800

    tomorrow: 551.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no551/StudentAttendanceRecordI.java
@@ -0,0 +1,12 @@
+package com.lzhlyle.leetcode.tomorrow.no551;
+
+public class StudentAttendanceRecordI {
+    public boolean checkRecord(String s) {
+        if (s.contains("LLL")) return false;
+        int a = 0;
+        for (char c : s.toCharArray()) {
+            if (c == 'A' && ++a == 2) return false;
+        }
+        return true;
+    }
+}
