e0db4f478d6c5bd296d3813bf9566e7f14d0ecb5
lzhlyle <lzhlyle@msn.cn>
2020-03-17 19:03:59 +0800

    tomorrow: 1071.

--- src/main/java/com/lzhlyle/leetcode/tomorrow/no1071/GreatestCommonDivisorOfStrings_Recursion.java
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no1071/GreatestCommonDivisorOfStrings_Recursion.java
@@ -2,10 +2,9 @@ package com.lzhlyle.leetcode.tomorrow.no1071;
 
 public class GreatestCommonDivisorOfStrings_Recursion {
     public String gcdOfStrings(String str1, String str2) {
-        int len1 = str1.length(), len2 = str2.length();
-        if (len2 == 0) return str1;
-        if (len2 > len1) return gcdOfStrings(str2, str1);
+        if (str2.isEmpty()) return str1;
+        if (str2.length() > str1.length()) return gcdOfStrings(str2, str1);
         if (str1.indexOf(str2) != 0) return "";
-        return gcdOfStrings(str1.substring(len2), str2);
+        return gcdOfStrings(str1.substring(str2.length()), str2);
     }
 }
