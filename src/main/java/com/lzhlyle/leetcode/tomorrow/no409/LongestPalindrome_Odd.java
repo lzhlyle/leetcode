9621330961a9fb286b85b96cfb78a7b0815a378d
lzhlyle <lzhlyle@msn.cn>
2020-03-20 21:56:51 +0800

    tomorrow: 409.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no409/LongestPalindrome_Odd.java
@@ -0,0 +1,11 @@
+package com.lzhlyle.leetcode.tomorrow.no409;
+
+public class LongestPalindrome_Odd {
+    public int longestPalindrome(String s) {
+        int[] freq = new int[58];
+        for (char c : s.toCharArray()) freq[c - 'A']++;
+        int odd = 0;
+        for (int f : freq) odd += f & 1;
+        return s.length() - odd + (odd > 0 ? 1 : 0);
+    }
+}
