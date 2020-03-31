0adf045dd6a4e405050c54ba8d4e65871a0e63b0
lzhlyle <lzhlyle@msn.cn>
2020-03-31 13:02:21 +0800

    recite and self: 912.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no912/SortAnArray_QuickSort.java
@@ -0,0 +1,30 @@
+package com.lzhlyle.leetcode.recite.no912;
+
+public class SortAnArray_QuickSort {
+    public int[] sortArray(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void quickSort(int[] arr, int l, int r) {
+        if (l >= r) return;
+        int p = partition(arr, l, r);
+        quickSort(arr, l, p - 1);
+        quickSort(arr, p + 1, r);
+    }
+
+    private int partition(int[] arr, int l, int r) {
+        int pivot = r, counter = l;
+        for (int i = l; i < r; i++) {
+            if (arr[i] < arr[pivot]) {
+                int swap = arr[i];
+                arr[i] = arr[counter];
+                arr[counter++] = swap;
+            }
+        }
+        int swap = arr[pivot];
+        arr[pivot] = arr[counter];
+        arr[counter] = swap;
+        return counter;
+    }
+}
