95a2ceaae1a0eb3a04a2309ee277d08f425aa37a
lzhlyle <lzhlyle@msn.cn>
2020-04-03 23:53:17 +0800

    tomorrow: 7.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no7/ReverseInteger_LC.java
@@ -0,0 +1,15 @@
+package com.lzhlyle.leetcode.tomorrow.no7;
+
+public class ReverseInteger_LC {
+    public int reverse(int x) {
+        int total = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            int tryTotal = total * 10 + digit;
+            if ((tryTotal - digit) / 10 != total) return 0;
+            total = tryTotal;
+            x /= 10;
+        }
+        return total;
+    }
+}
