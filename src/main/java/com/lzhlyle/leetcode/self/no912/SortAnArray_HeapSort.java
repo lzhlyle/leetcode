fa2f145d02a2d93ae179cbd4f97fe187b03ca107
lzhlyle <lzhlyle@msn.cn>
2020-04-03 12:33:32 +0800

    Update HeapSort.

--- src/main/java/com/lzhlyle/leetcode/self/no912/SortAnArray_HeapSort.java
+++ src/main/java/com/lzhlyle/leetcode/self/no912/SortAnArray_HeapSort.java
@@ -10,10 +10,9 @@ public class SortAnArray_HeapSort {
         if (arr == null || arr.length < 2) return;
         int len = arr.length;
         for (int i = len / 2 - 1; i >= 0; i--) heapify(arr, len, i);
+
         for (int i = len - 1; i >= 0; i--) {
-            int swap = arr[0];
-            arr[0] = arr[i];
-            arr[i] = swap;
+            int swap = arr[0]; arr[0] = arr[i]; arr[i] = swap;
             heapify(arr, i, 0);
         }
     }
@@ -23,9 +22,8 @@ public class SortAnArray_HeapSort {
         if (l < len && arr[l] > arr[max]) max = l;
         if (r < len && arr[r] > arr[max]) max = r;
         if (max == top) return;
-        int swap = arr[max];
-        arr[max] = arr[top];
-        arr[top] = swap;
+
+        int swap = arr[max]; arr[max] = arr[top]; arr[top] = swap;
         heapify(arr, len, max);
     }
 }
--- src/main/java/com/lzhlyle/templates/algorithm/sort/HeapSort.java
+++ src/main/java/com/lzhlyle/templates/algorithm/sort/HeapSort.java
@@ -3,19 +3,12 @@ package com.lzhlyle.templates.algorithm.sort;
 import java.util.Arrays;
 
 public class HeapSort {
-    public static void heapSort(int[] arr) {
-        if (arr == null || arr.length <= 1) return;
+    private static void heapSort(int[] arr) {
+        if (arr == null || arr.length < 2) return;
+        int len = arr.length;
+        for (int i = len / 2 - 1; i >= 0; i--) heapify(arr, len, i);
 
-        // 建立大顶堆 - 通过数组维护
-        int length = arr.length;
-        // 只需比较一半，heapify中已同时比较左右
-        // 堆底 length / 2 - 1
-        for (int i = length / 2 - 1; i >= 0; i--) {
-            heapify(arr, length, i);
-        }
-
-        // 将堆顶取出，并换到最后
-        for (int i = length - 1; i >= 0; i--) {
+        for (int i = len - 1; i >= 0; i--) {
             int swap = arr[0];
             arr[0] = arr[i];
             arr[i] = swap;
@@ -23,33 +16,16 @@ public class HeapSort {
         }
     }
 
-    /**
-     * 堆化，调整堆
-     *
-     * @param arr    用来维护堆的数组
-     * @param length 数组中未排序的长度，保证寻找子集时不超出堆范围
-     * @param top    堆顶元素在数组中的索引
-     */
-    private static void heapify(int[] arr, int length, int top) {
-        int left = 2 * top + 1, right = 2 * top + 2;
-        int largest = top;
+    private static void heapify(int[] arr, int len, int top) {
+        int l = 2 * top + 1, r = l + 1, max = top;
+        if (l < len && arr[l] > arr[max]) max = l;
+        if (r < len && arr[r] > arr[max]) max = r;
+        if (max == top) return;
 
-        // 同时比较左右
-        // largest = max(arr[top], arr[left], arr[right]);
-        if (left < length && arr[left] > arr[largest]) largest = left;
-        if (right < length && arr[right] > arr[largest]) largest = right;
-
-        // terminator
-        // 当前层，堆顶已经最大
-        if (largest == top) return;
-
-        // process
-        int swap = arr[top];
-        arr[top] = arr[largest];
-        arr[largest] = swap;
-
-        // drill down
-        heapify(arr, length, largest);
+        int swap = arr[max];
+        arr[max] = arr[top];
+        arr[top] = swap;
+        heapify(arr, len, max);
     }
 
     public static void main(String[] args) {
