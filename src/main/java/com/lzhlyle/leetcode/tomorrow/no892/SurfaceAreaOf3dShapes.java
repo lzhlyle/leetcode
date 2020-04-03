a208a2774e1ae9b6c5fb5e2e0968a00e23644310
lzhlyle <lzhlyle@msn.cn>
2020-04-03 23:46:11 +0800

    tomorrow: 892.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no892/SurfaceAreaOf3dShapes.java
@@ -0,0 +1,18 @@
+package com.lzhlyle.leetcode.tomorrow.no892;
+
+public class SurfaceAreaOf3dShapes {
+    public int surfaceArea(int[][] grid) {
+        int n = grid.length, res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int h = grid[i][j];
+                if (h > 0) { // ..
+                    res += 4 * h + 2;
+                    if (i > 0 && grid[i - 1][j] > 0) res -= Math.min(h, grid[i - 1][j]) * 2;
+                    if (j > 0 && grid[i][j - 1] > 0) res -= Math.min(h, grid[i][j - 1]) * 2;
+                }
+            }
+        }
+        return res;
+    }
+}
