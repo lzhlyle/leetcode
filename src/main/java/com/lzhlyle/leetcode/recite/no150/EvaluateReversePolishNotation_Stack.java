1ff3d77b837fe1a51641a9c3254a074279d72619
lzhlyle <lzhlyle@msn.cn>
2020-04-04 15:42:58 +0800

    recite and self: 150.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no150/EvaluateReversePolishNotation_Stack.java
@@ -0,0 +1,32 @@
+package com.lzhlyle.leetcode.recite.no150;
+
+import java.util.Stack;
+
+public class EvaluateReversePolishNotation_Stack {
+    public int evalRPN(String[] tokens) {
+        String opers = "+-*/";
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            if (opers.contains(token)) {
+                int b = stack.pop(), a = stack.pop();
+                int res = calc(a, b, token);
+                stack.push(res);
+            } else stack.push(Integer.valueOf(token));
+        }
+        return stack.pop();
+    }
+
+    private int calc(int a, int b, String oper) {
+        switch (oper) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b;
+        }
+        throw new RuntimeException("Unknown operation: " + oper);
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/self/no150/EvaluateReversePolishNotation_LC_Array.java
@@ -0,0 +1,18 @@
+package com.lzhlyle.leetcode.self.no150;
+
+public class EvaluateReversePolishNotation_LC_Array {
+    public int evalRPN(String[] tokens) {
+        int[] arr = new int[tokens.length / 2 + 1];
+        int i = 0;
+        for (String token : tokens) {
+            switch (token) {
+                case "+" : arr[i - 2] += arr[--i]; break;
+                case "-" : arr[i - 2] -= arr[--i]; break;
+                case "*" : arr[i - 2] *= arr[--i]; break;
+                case "/" : arr[i - 2] /= arr[--i]; break;
+                default: arr[i++] = Integer.parseInt(token);
+            }
+        }
+        return arr[0];
+    }
+}
