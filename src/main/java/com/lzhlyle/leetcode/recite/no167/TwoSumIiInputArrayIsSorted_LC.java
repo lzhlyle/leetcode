f59860151b5dc895c5d67d4941a0de47159157ad
lzhlyle <lzhlyle@msn.cn>
2020-04-09 00:22:53 +0800

    recite and self: 167.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no167/TwoSumIiInputArrayIsSorted_LC.java
@@ -0,0 +1,12 @@
+package com.lzhlyle.leetcode.recite.no167;
+
+public class TwoSumIiInputArrayIsSorted_LC {
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0, r = numbers.length - 1;
+        while (numbers[l] + numbers[r] != target) {
+            if (numbers[l] + numbers[r] < target) l++;
+            else r--;
+        }
+        return new int[]{l + 1, r + 1};
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/self/no167/TwoSumIiInputArrayIsSorted.java
@@ -0,0 +1,14 @@
+package com.lzhlyle.leetcode.self.no167;
+
+public class TwoSumIiInputArrayIsSorted {
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0, r = numbers.length - 1;
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum == target) return new int[]{l + 1, r + 1};
+            if (sum < target) l++;
+            else r--;
+        }
+        return null;
+    }
+}
