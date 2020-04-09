a7acee82f795b6ab830a7fb264280138441dfa46
lzhlyle <lzhlyle@msn.cn>
2020-04-09 23:11:28 +0800

    Update week: 22.

--- src/main/java/com/lzhlyle/leetcode/week/no22/GenerateParentheses_DFS.java
+++ src/main/java/com/lzhlyle/leetcode/week/no22/GenerateParentheses_DFS.java
@@ -1,15 +1,12 @@
 package com.lzhlyle.leetcode.week.no22;
 
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GenerateParentheses_DFS {
     public List<String> generateParenthesis(int n) {
-        if (n < 1) return Collections.emptyList();
-        if (n == 1) return Collections.singletonList("()");
-        List<String> res = new LinkedList<>();
-        dfs(n, 1, "(", res);
+        List<String> res = new ArrayList<>();
+        dfs(n, 0, "", res);
         return res;
     }
 
