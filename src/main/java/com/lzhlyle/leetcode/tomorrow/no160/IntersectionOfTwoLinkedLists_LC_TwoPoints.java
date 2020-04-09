1d12e9b6c6d8c261cbb7a9edf274be0309ecc1e4
lzhlyle <lzhlyle@msn.cn>
2020-04-09 23:47:16 +0800

    tomorrow: 160.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no160/IntersectionOfTwoLinkedLists_LC_TwoPoints.java
@@ -0,0 +1,13 @@
+package com.lzhlyle.leetcode.tomorrow.no160;
+
+public class IntersectionOfTwoLinkedLists_LC_TwoPoints {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode a = headA, b = headB;
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+}
