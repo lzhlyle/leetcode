316f0583a5ec51059cf796ab8f243754b3e54762
lzhlyle <lzhlyle@msn.cn>
2020-04-10 14:59:10 +0800

    tomorrow: 820.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no820/Trie.java
@@ -0,0 +1,25 @@
+package com.lzhlyle.leetcode.tomorrow.no820;
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public boolean insert(String word) {
+        boolean isNew = false;
+        char[] chars = word.toCharArray();
+        TrieNode curr = root;
+        for (int i = chars.length - 1; i >= 0; i--) {
+            char c = chars[i];
+            if (!curr.contains(c)) {
+                curr.set(c, new TrieNode());
+                isNew = true;
+            }
+            curr = curr.get(c);
+        }
+        curr.setEnd();
+        return isNew;
+    }
+}
