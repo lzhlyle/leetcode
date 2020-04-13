362e6d0b371a9435773eafd3b10d40cdc0c01b54
lzhlyle <lzhlyle@msn.cn>
2020-04-13 16:29:40 +0800

    recite and self: 525.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no525/ContiguousArray_LC_Map.java
@@ -0,0 +1,20 @@
+package com.lzhlyle.leetcode.recite.no525;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContiguousArray_LC_Map {
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int cnt = 0, max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) cnt++;
+            else cnt--;
+
+            if (!map.containsKey(cnt)) map.put(cnt, i);
+            else max = Math.max(max, i - map.get(cnt));
+        }
+        return max;
+    }
+}
