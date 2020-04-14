8615716567124f30d1e3063d6e3e3d0e4fe04c96
lzhlyle <lzhlyle@msn.cn>
2020-04-15 00:37:03 +0800

    recite and self: 354.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no354/RussianDollEnvelopes.java
@@ -0,0 +1,31 @@
+package com.lzhlyle.leetcode.recite.no354;
+
+import java.util.Arrays;
+
+public class RussianDollEnvelopes {
+    // sort + lis
+    // dp + bs
+    public int maxEnvelopes(int[][] envelopes) {
+        int len = envelopes.length;
+        Arrays.sort(envelopes, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+
+        // lis
+        int[] groups = new int[len];
+        int cnt = 0;
+        for (int[] pair : envelopes) {
+            int i = bs(groups, pair[1], 0, cnt);
+            groups[i] = pair[1];
+            if (i == cnt) cnt++;
+        }
+        return cnt;
+    }
+
+    private int bs(int[] arr, int target, int l, int r) {
+        while (l < r) {
+            int mid = l + ((r - l) >> 1);
+            if (arr[mid] < target) l = mid + 1;
+            else r = mid;
+        }
+        return l;
+    }
+}
