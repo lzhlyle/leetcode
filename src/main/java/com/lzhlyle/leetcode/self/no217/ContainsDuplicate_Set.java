7c0ee4eaddd435cf5d79d3980d0b97a8fdb09c48
lzhlyle <lzhlyle@msn.cn>
2020-04-15 22:34:19 +0800

    recite and self: 217.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no217/ContainsDuplicate_Set.java
@@ -0,0 +1,13 @@
+package com.lzhlyle.leetcode.self.no217;
+
+import java.util.Arrays;
+
+public class ContainsDuplicate_Set {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/self/no217/ContainsDuplicate_Sort.java
@@ -0,0 +1,15 @@
+package com.lzhlyle.leetcode.self.no217;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate_Sort {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            if (set.contains(n)) return true;
+            set.add(n);
+        }
+        return false;
+    }
+}
