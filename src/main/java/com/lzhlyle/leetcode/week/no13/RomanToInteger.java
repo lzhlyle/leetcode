c1d963ab6e93a23012bdea8a428ba080f99b8c77
lzhlyle <lzhlyle@msn.cn>
2020-04-17 20:40:40 +0800

    week: 13.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no13/RomanToInteger.java
@@ -0,0 +1,25 @@
+package com.lzhlyle.leetcode.week.no13;
+
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        int[] map = init();
+        char[] arr = s.toCharArray();
+        int res = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int num = map[arr[i] - 'A'], next = map[arr[i + 1] - 'A'];
+            if (num < next) res -= num;
+            else res += num;
+        }
+        return res + map[arr[arr.length - 1] - 'A'];
+    }
+
+    private int[] init() {
+        int[] map = new int[26];
+        char[] chars = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+        int[] arr = {1, 5, 10, 50, 100, 500, 1000};
+        for (int i = 0; i < 7; i++) {
+            map[chars[i] - 'A'] = arr[i];
+        }
+        return map;
+    }
+}
