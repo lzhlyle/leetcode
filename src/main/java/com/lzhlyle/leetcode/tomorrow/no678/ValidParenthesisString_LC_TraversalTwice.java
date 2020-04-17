8fcfcfa7e692c87abc8d51c720ea85a366954c6f
lzhlyle <lzhlyle@msn.cn>
2020-04-18 00:08:29 +0800

    tomorrow: 678.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no678/ValidParenthesisString_LC_TraversalTwice.java
@@ -0,0 +1,23 @@
+package com.lzhlyle.leetcode.tomorrow.no678;
+
+public class ValidParenthesisString_LC_TraversalTwice {
+    public boolean checkValidString(String s) {
+        // all * for (, valid enough or not
+        int l = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c != ')') l++;
+            else if (l-- == 0) return false;
+        }
+        if (l == 0) return true;
+
+        // all * for ), valid enough or not
+        int r = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if (c != '(') r++;
+            else if (r-- == 0) return false;
+        }
+        return true;
+    }
+}
