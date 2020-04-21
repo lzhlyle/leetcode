5310c9e88a35f4458ef7b3aa3832a96a296bf0ab
lzhlyle <lzhlyle@msn.cn>
2020-04-22 00:20:34 +0800

    recite and self: 234.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no234/ListNode.java
@@ -0,0 +1,10 @@
+package com.lzhlyle.leetcode.recite.no234;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/recite/no234/PalindromeLinkedList_LC_Reverse.java
@@ -0,0 +1,25 @@
+package com.lzhlyle.leetcode.recite.no234;
+
+public class PalindromeLinkedList_LC_Reverse {
+    // 反转前半段 O(n), O(1)
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+        ListNode slow = head, fast = head;
+        ListNode pre = head, prepre = null;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+            pre.next = prepre;
+            prepre = pre;
+        }
+        if (fast != null) slow = slow.next;
+        while (pre != null && slow != null) {
+            if (pre.val != slow.val) return false;
+            pre = pre.next;
+            slow = slow.next;
+        }
+        // 还应反转回去
+        return true;
+    }
+}
