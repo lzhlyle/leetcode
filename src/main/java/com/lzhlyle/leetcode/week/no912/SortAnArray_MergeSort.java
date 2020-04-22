b2f91b581700c97723ab72f418a47d396a66b17c
lzhlyle <lzhlyle@msn.cn>
2020-04-22 16:36:31 +0800

    week: 912.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no912/SortAnArray_MergeSort.java
@@ -0,0 +1,27 @@
+package com.lzhlyle.leetcode.week.no912;
+
+public class SortAnArray_MergeSort {
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void mergeSort(int[] arr, int l, int r) {
+        if (l >= r) return;
+        int mid = l + ((r - l) >> 1);
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+
+    private void merge(int[] arr, int l, int mid, int r) {
+        int[] temp = new int[r - l + 1];
+        int i = l, j = mid + 1, k = 0;
+        while (i <= mid && j <= r) {
+            temp[k++] = arr[i] < arr[j] ? arr[i++] : arr[j++];
+        }
+        while (i <= mid) temp[k++] = arr[i++];
+        while (j <= r) temp[k++] = arr[j++];
+        System.arraycopy(temp, 0, arr, l, r - l + 1);
+    }
+}
