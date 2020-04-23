a9c55cc007db3232d6eaf3adc368011725d8d380
lzhlyle <lzhlyle@msn.cn>
2020-04-23 22:30:58 +0800

    recite and self: 237.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no237/DeleteNodeInALinkedList.java
@@ -0,0 +1,8 @@
+package com.lzhlyle.leetcode.self.no237;
+
+public class DeleteNodeInALinkedList {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
