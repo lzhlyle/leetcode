3751faa14e7c8be94d635d503fffec2d988b7944
lzhlyle <lzhlyle@msn.cn>
2020-04-28 18:40:15 +0800

    recite and self: 137.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no137/SingleNumberIi_LC_Bitwise.java
@@ -0,0 +1,12 @@
+package com.lzhlyle.leetcode.recite.no137;
+
+public class SingleNumberIi_LC_Bitwise {
+    public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0;
+        for (int num : nums) {
+            ones = ones ^ num & ~twos;
+            twos = twos ^ num & ~ones;
+        }
+        return ones;
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/self/no137/SingleNumberIi_Bitwise.java
@@ -0,0 +1,16 @@
+package com.lzhlyle.leetcode.self.no137;
+
+public class SingleNumberIi_Bitwise {
+    public int singleNumber(int[] nums) {
+        // every bit should be 3 times of '1'
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            int cnt = 0;
+            for (int num : nums) {
+                if (((num >> i) & 1) == 1) cnt++;
+            }
+            if (cnt % 3 != 0) res |= (1 << i);
+        }
+        return res;
+    }
+}
