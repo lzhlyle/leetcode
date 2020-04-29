039f51dc31844b4c84e3ef763f98a22f287abc09
lzhlyle <lzhlyle@msn.cn>
2020-04-29 20:14:36 +0800

    recite and self: 124.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no124/BinaryTreeMaximumPathSum_DFS.java
@@ -0,0 +1,17 @@
+package com.lzhlyle.leetcode.recite.no124;
+
+public class BinaryTreeMaximumPathSum_DFS {
+    private int max = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode curr) {
+        if (curr == null) return 0;
+        int l = Math.max(0, dfs(curr.left)), r = Math.max(0, dfs(curr.right));
+        max = Math.max(max, curr.val + l + r); // 以 curr 为顶点时
+        return curr.val + Math.max(l, r); // 以 curr 之上为顶点，则只能 l / r
+    }
+}
