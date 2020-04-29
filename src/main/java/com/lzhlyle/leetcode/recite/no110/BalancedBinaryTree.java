4fe577e786bdc4407c4bd5bf412d888dfe6d39a9
lzhlyle <lzhlyle@msn.cn>
2020-04-29 20:44:41 +0800

    recite and self: 110.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no110/BalancedBinaryTree.java
@@ -0,0 +1,17 @@
+package com.lzhlyle.leetcode.recite.no110;
+
+public class BalancedBinaryTree {
+    private boolean isBalanced = true;
+
+    public boolean isBalanced(TreeNode root) {
+        depth(root);
+        return isBalanced;
+    }
+
+    private int depth(TreeNode curr) {
+        if (curr == null || !isBalanced) return 0;
+        int l = depth(curr.left), r = depth(curr.right);
+        isBalanced = isBalanced && Math.abs(l - r) < 2;
+        return Math.max(l, r) + 1;
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/self/no110/TreeNode.java
@@ -0,0 +1,11 @@
+package com.lzhlyle.leetcode.self.no110;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
