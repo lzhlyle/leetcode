9f6420982ec2bdf18f6f28bac68e4519a7fe8b8f
lzhlyle <lzhlyle@msn.cn>
2020-05-01 20:34:49 +0800

    recite and self: 278.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no278/FirstBadVersion_BS.java
@@ -0,0 +1,17 @@
+package com.lzhlyle.leetcode.recite.no278;
+
+public class FirstBadVersion_BS {
+    public int firstBadVersion(int n) {
+        int l = 1, r = n;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (!isBadVersion(mid)) l = mid + 1;
+            else r = mid;
+        }
+        return l;
+    }
+
+    static boolean isBadVersion(int version) {
+        return false;
+    }
+}
