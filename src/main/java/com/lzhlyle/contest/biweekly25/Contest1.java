dfcc202f9baa21f4883c4a49967ef70222db3dab
lzhlyle <lzhlyle@msn.cn>
2020-05-03 00:29:15 +0800

    contest: biweekly 25.

---
+++ src/main/java/com/lzhlyle/contest/biweekly25/Contest1.java
@@ -0,0 +1,12 @@
+package com.lzhlyle.contest.biweekly25;
+
+public class Contest1 {
+
+
+    public static void main(String[] args) {
+        Contest1 contest = new Contest1();
+        {
+            
+        }
+    }
+}
---
+++ src/main/java/com/lzhlyle/contest/biweekly25/Contest4.java
@@ -0,0 +1,38 @@
+package com.lzhlyle.contest.biweekly25;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Contest4 {
+    // wrong
+    // 数独??
+    // greedy + dp
+    private static final int _MOD = 1000000007;
+
+    public int numberWays(List<List<Integer>> hats) {
+        // 排序，先安排最挑食的
+        hats.sort(Comparator.comparingInt(List::size));
+
+        int n = hats.size(), sum = 0;
+        long[][] dp = new long[n][40]; // i:人, j:已选的帽子, val:方案数
+        List<Integer> firstLike = hats.get(0);
+        for (int j = 0; j < firstLike.size(); j++) dp[0][firstLike.get(j) - 1] = 1;
+        for (int i = 1; i < n; i++) {
+            List<Integer> likes = hats.get(i);
+            for (int j = 0; j < likes.size(); j++) {
+                int h = likes.get(j) - 1;
+                if (dp[i - 1][h] > 0) continue;
+                dp[i][j] = dp[i - 1][h];
+                sum += dp[i][h] % _MOD;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Contest4 contest = new Contest4();
+        {
+
+        }
+    }
+}
