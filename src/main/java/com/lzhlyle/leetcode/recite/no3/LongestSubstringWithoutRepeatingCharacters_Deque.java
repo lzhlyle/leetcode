c6ca3d046d84edf6751eb89f57d772faec9cd74c
lzhlyle <lzhlyle@msn.cn>
2020-05-02 15:10:10 +0800

    recite and self: 3.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no3/LongestSubstringWithoutRepeatingCharacters_Deque.java
@@ -0,0 +1,23 @@
+package com.lzhlyle.leetcode.recite.no3;
+
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
+public class LongestSubstringWithoutRepeatingCharacters_Deque {
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        Set<Character> set = new HashSet<>();
+        Deque<Character> deque = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            while (set.contains(c)) {
+                set.remove(deque.removeFirst());
+            }
+            set.add(c);
+            deque.addLast(c);
+            max = Math.max(max, deque.size());
+        }
+        return max;
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/self/no3/LongestSubstringWithoutRepeatingCharacters_TwoPoints.java
@@ -0,0 +1,18 @@
+package com.lzhlyle.leetcode.self.no3;
+
+public class LongestSubstringWithoutRepeatingCharacters_TwoPoints {
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        boolean[] visited = new boolean[256]; // instead of Set
+        char[] arr = s.toCharArray();
+        for (int l = 0, r = 0; r < arr.length; r++) {
+            char c = arr[r];
+                while (visited[c]) {
+                    visited[arr[l++]] = false;
+                }
+            visited[c] = true;
+            max = Math.max(max, r - l + 1);
+        }
+        return max;
+    }
+}
