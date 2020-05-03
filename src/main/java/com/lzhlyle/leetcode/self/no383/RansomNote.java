356fb505fced1e6f52b75a966186cb73dae8a0eb
lzhlyle <lzhlyle@msn.cn>
2020-05-03 19:43:50 +0800

    recite and self: 383.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no383/RansomNote.java
@@ -0,0 +1,13 @@
+package com.lzhlyle.leetcode.self.no383;
+
+public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) return false;
+        int[] freq = new int[26];
+        for (char c : magazine.toCharArray()) freq[c - 'a']++;
+        for (char c : ransomNote.toCharArray()) {
+            if (freq[c - 'a']-- == 0) return false;
+        }
+        return true;
+    }
+}
