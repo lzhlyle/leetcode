863bfbc73f559f19cd3e450cab7711089e8a6ad9
lzhlyle <lzhlyle@msn.cn>
2020-05-04 23:18:52 +0800

    recite and self: 106.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no106/ConstructBinaryTreeFromInorderAndPostorderTraversal_DC.java
@@ -0,0 +1,27 @@
+package com.lzhlyle.leetcode.recite.no106;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConstructBinaryTreeFromInorderAndPostorderTraversal_DC {
+    // recursion
+    private Map<Integer, Integer> map = new HashMap<>(); // (inorder val, inorder index))
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        int len = inorder.length;
+        if (len == 0) return null;
+        for (int i = 0; i < len; i++) map.put(inorder[i], i);
+        return _build(inorder, 0, len - 1, postorder, 0, len - 1);
+    }
+
+    private TreeNode _build(int[] inorder, int il, int ir, int[] postorder, int pl, int pr) {
+        // look up ROOT NODE first
+        TreeNode root = new TreeNode(postorder[pr]);
+        int pivot = map.get(root.val); // root index in in-order array;
+        int leftCnt = pivot - il;
+
+        root.left = pivot == il ? null : _build(inorder, il, pivot - 1, postorder, pl, pl + leftCnt - 1);
+        root.right = pivot == ir ? null : _build(inorder, pivot + 1, ir, postorder, pl + leftCnt, pr - 1);
+        return root;
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/recite/no106/TreeNode.java
@@ -0,0 +1,11 @@
+package com.lzhlyle.leetcode.recite.no106;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
