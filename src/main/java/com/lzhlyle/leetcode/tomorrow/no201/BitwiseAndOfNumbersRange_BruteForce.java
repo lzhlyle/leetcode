861aa9d66ba11b60932b943e94d2047cc5bdb22f
lzhlyle <lzhlyle@msn.cn>
2020-05-06 00:43:40 +0800

    tomorrow: 201.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no201/BitwiseAndOfNumbersRange_BruteForce.java
@@ -0,0 +1,12 @@
+package com.lzhlyle.leetcode.tomorrow.no201;
+
+public class BitwiseAndOfNumbersRange_BruteForce {
+    public int rangeBitwiseAnd(int m, int n) {
+        int res = m;
+        for (int i = m + 1; i >= m && i <= n; i++) {
+            res &= i;
+            if (res == 0) return 0;
+        }
+        return res;
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no201/BitwiseAndOfNumbersRange_LC_Adv1.java
@@ -0,0 +1,14 @@
+package com.lzhlyle.leetcode.tomorrow.no201;
+
+public class BitwiseAndOfNumbersRange_LC_Adv1 {
+    // 右移直到相等
+    public int rangeBitwiseAnd(int m, int n) {
+        int cnt = 0;
+        while (n > m) {
+            n >>>= 1;
+            m >>>= 1;
+            cnt++;
+        }
+        return m << cnt;
+    }
+}
