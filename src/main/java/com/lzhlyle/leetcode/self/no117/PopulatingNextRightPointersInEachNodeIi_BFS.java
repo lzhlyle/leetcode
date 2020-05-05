32122721586e17707772e7b51dc0bdc995e21abf
lzhlyle <lzhlyle@msn.cn>
2020-05-05 15:47:05 +0800

    recite and self: 117.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no117/PopulatingNextRightPointersInEachNodeIi_BFS.java
@@ -0,0 +1,24 @@
+package com.lzhlyle.leetcode.self.no117;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PopulatingNextRightPointersInEachNodeIi_BFS {
+    // bfs
+    public Node connect(Node root) {
+        if (root == null) return null;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Queue<Node> next = new LinkedList<>();
+            while (!queue.isEmpty()) {
+                Node node = queue.remove();
+                if (!queue.isEmpty()) node.next = queue.element();
+                if (node.left != null) next.add(node.left);
+                if (node.right != null) next.add(node.right);
+            }
+            queue = next;
+        }
+        return root;
+    }
+}
