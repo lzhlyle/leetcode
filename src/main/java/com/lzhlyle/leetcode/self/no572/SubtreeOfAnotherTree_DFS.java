fcf440726457bc5a4bc8f73c999f463c58ea3f65
lzhlyle <lzhlyle@msn.cn>
2020-05-08 00:37:35 +0800

    recite and self: 572.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no572/SubtreeOfAnotherTree_DFS.java
@@ -0,0 +1,28 @@
+package com.lzhlyle.leetcode.self.no572;
+
+import java.util.Stack;
+
+public class SubtreeOfAnotherTree_DFS {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        Stack<TreeNode> stack = find(s, t.val, new Stack<>());
+        while (!stack.isEmpty()) {
+            if (valid(stack.pop(), t)) return true;
+        }
+        return false;
+    }
+
+    private boolean valid(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+        if (s.val != t.val) return false;
+        return valid(s.left, t.left) && valid(s.right, t.right);
+    }
+
+    private Stack<TreeNode> find(TreeNode root, int target, Stack<TreeNode> stack) {
+        if (root == null) return stack;
+        if (root.val == target) stack.push(root);
+        find(root.left, target, stack);
+        find(root.right, target, stack);
+        return stack;
+    }
+}
