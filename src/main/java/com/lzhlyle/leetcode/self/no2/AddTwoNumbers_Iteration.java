e2eec70669de508ea934633c0af9804f12324449
lzhlyle <lzhlyle@msn.cn>
2020-05-07 16:44:28 +0800

    recite and self: 2.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no2/AddTwoNumbers_Iteration.java
@@ -0,0 +1,23 @@
+package com.lzhlyle.leetcode.self.no2;
+
+public class AddTwoNumbers_Iteration {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(-1), curr = head;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            if (l1 == null) l1 = new ListNode(0);
+            if (l2 == null) l2 = new ListNode(0);
+
+            int n1 = l1.val, n2 = l2.val, sum = n1 + n2 + carry;
+
+            curr.next = new ListNode(sum % 10);
+
+            carry = sum > 9 ? 1 : 0;
+            curr = curr.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        if (carry > 0) curr.next = new ListNode(carry);
+        return head.next;
+    }
+}
