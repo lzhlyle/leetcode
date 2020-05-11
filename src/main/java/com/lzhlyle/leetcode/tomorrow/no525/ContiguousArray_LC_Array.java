30d227717249e1c3ee31cd08b264ba3b9d11bf9e
lzhlyle <lzhlyle@msn.cn>
2020-05-11 22:32:10 +0800

    tomorrow: 525.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no525/ContiguousArray_LC_Array.java
@@ -0,0 +1,20 @@
+package com.lzhlyle.leetcode.tomorrow.no525;
+
+import java.util.Arrays;
+
+public class ContiguousArray_LC_Array {
+    public int findMaxLength(int[] nums) {
+        int cnt = 0, max = 0, len = nums.length;
+        int[] arr = new int[2 * len + 1];
+        Arrays.fill(arr, -2);
+        arr[len] = -1;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] == 1) cnt++;
+            else cnt--;
+
+            if (arr[cnt + len] == -2) arr[cnt + len] = i;
+            else max = Math.max(max, i - arr[cnt + len]);
+        }
+        return max;
+    }
+}
