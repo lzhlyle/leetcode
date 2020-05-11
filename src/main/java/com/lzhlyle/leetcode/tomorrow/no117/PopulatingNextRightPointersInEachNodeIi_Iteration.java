3527c8de6e2f8e101abe55bcce2c1cca89c71195
lzhlyle <lzhlyle@msn.cn>
2020-05-11 23:39:21 +0800

    tomorrow: 117.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no117/PopulatingNextRightPointersInEachNodeIi_Iteration.java
@@ -0,0 +1,19 @@
+package com.lzhlyle.leetcode.tomorrow.no117;
+
+public class PopulatingNextRightPointersInEachNodeIi_Iteration {
+    // curr 为上一层
+    // head, tail 为下一层，每次都新开
+    public Node connect(Node root) {
+        Node curr = root;
+        while (curr != null) {
+            Node head = new Node(), tail = head;
+            while (curr != null) {
+                if (curr.left != null) tail = tail.next = curr.left;
+                if (curr.right != null) tail = tail.next = curr.right;
+                curr = curr.next; // curr 当前层的下一个
+            }
+            curr = head.next; // curr 的下一层
+        }
+        return root;
+    }
+}
