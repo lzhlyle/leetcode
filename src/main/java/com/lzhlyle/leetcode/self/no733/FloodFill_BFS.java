f76062d8da605db42a205a7ba9fb8e5a15098dd5
lzhlyle <lzhlyle@msn.cn>
2020-05-11 17:52:21 +0800

    recite and self: 733.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no733/FloodFill_BFS.java
@@ -0,0 +1,32 @@
+package com.lzhlyle.leetcode.self.no733;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FloodFill_BFS {
+    private static final int[] dx = {-1, 0, 1, 0}, dy = {0, -1, 0, 1};
+    private int m, n;
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int target = image[sr][sc];
+        if (target == newColor) return image;
+
+        m = image.length;
+        n = image[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{sr, sc});
+        while (!queue.isEmpty()) {
+            int[] p = queue.remove();
+            image[p[0]][p[1]] = newColor;
+            for (int di = 0; di < 4; di++) {
+                int x = p[0] + dx[di], y = p[1] + dy[di];
+                if (x >= 0 && x < m && y >= 0 && y < n)
+                    if (image[x][y] == target) {
+                        image[x][y] = -1;
+                        queue.add(new int[]{x, y});
+                    }
+            }
+        }
+        return image;
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/self/no733/FloodFill_DFS.java
@@ -0,0 +1,26 @@
+package com.lzhlyle.leetcode.self.no733;
+
+public class FloodFill_DFS {
+    private static final int[] dx = {-1, 0, 1, 0}, dy = {0, -1, 0, 1};
+    private int m, n;
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if (image[sr][sc] == newColor) return image;
+        m = image.length;
+        n = image[0].length;
+        dfs(image, sr, sc, image[sr][sc], newColor, new boolean[m][n]);
+        return image;
+    }
+
+    private void dfs(int[][] image, int i, int j, int from, int to, boolean[][] visited) {
+        if (image[i][j] != from || visited[i][j]) return;
+        visited[i][j] = true;
+        image[i][j] = to;
+        for (int di = 0; di < 4; di++) {
+            int x = i + dx[di], y = j + dy[di];
+            if (x >= 0 && x < m && y >= 0 && y < n)
+                if (image[x][y] == from && !visited[x][y])
+                    dfs(image, x, y, from, to, visited);
+        }
+    }
+}
