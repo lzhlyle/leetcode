c3b8a51b4e5f4e074cfe39be3ac0876716fb5f47
lzhlyle <lzhlyle@msn.cn>
2020-05-15 00:21:04 +0800

    week: 560.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no560/SubarraySumEqualsK.java
@@ -0,0 +1,19 @@
+package com.lzhlyle.leetcode.week.no560;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumEqualsK {
+    // prefix sum
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int cnt = 0, sum = 0;
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) cnt += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return cnt;
+    }
+}
