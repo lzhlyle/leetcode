6f41e922a3694c50c067614ce1416526c6e40837
lzhlyle <lzhlyle@msn.cn>
2020-05-31 19:35:31 +0800

    week: 48.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no48/RotateImage_IJ_RowReverse.java
@@ -0,0 +1,25 @@
+package com.lzhlyle.leetcode.week.no48;
+
+public class RotateImage_IJ_RowReverse {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+         // i <-> j
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int swap = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = swap;
+            }
+        }
+
+        // reverse each row
+        for (int[] row : matrix) {
+            int l = 0, r = n - 1;
+            while (l < r) {
+                int swap = row[l];
+                row[l++] = row[r];
+                row[r--] = swap;
+            }
+        }
+    }
+}
