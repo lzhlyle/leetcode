cc7c19a1a1a9551671e0ec39ee67f3cfd0b51349
lzhlyle <lzhlyle@msn.cn>
2020-06-02 23:19:24 +0800

    week: 1095.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no1095/FindInMountainArray.java
@@ -0,0 +1,34 @@
+package com.lzhlyle.leetcode.week.no1095;
+
+public class FindInMountainArray {
+    // bs O(log(n))
+    public int findInMountainArray(int target, MountainArray ma) {
+        int len = ma.length(), l = 0, r = len - 1;
+        // looking for the INDEX of the peek
+        while (l < r) {
+            int mid = l + ((r - l + 1) >> 1);
+            if (ma.get(mid - 1) < ma.get(mid)) l = mid;
+            else r = mid - 1;
+        }
+        int top = l;
+        if (ma.get(top) == target) return top;
+
+        l = 0;
+        r = top;
+        while (l < r) {
+            int mid = l + ((r - l) >> 1);
+            if (ma.get(mid) < target) l = mid + 1;
+            else r = mid;
+        }
+        if (ma.get(l) == target) return l;
+
+        l = top;
+        r = len - 1;
+        while (l < r) {
+            int mid = l + ((r - l) >> 1);
+            if (ma.get(mid) > target) l = mid + 1;
+            else r = mid;
+        }
+        return ma.get(l) == target ? l : -1;
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/week/no1095/MountainArray.java
@@ -0,0 +1,7 @@
+package com.lzhlyle.leetcode.week.no1095;
+
+public interface MountainArray {
+    int get(int index);
+
+    int length();
+}
