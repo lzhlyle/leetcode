135edcdfee7ee55658af92ba8c2e1a82ced9ccec
lzhlyle <lzhlyle@msn.cn>
2020-06-09 14:24:54 +0800

    week: 476.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no476/NumberComplement_Adv.java
@@ -0,0 +1,10 @@
+package com.lzhlyle.leetcode.week.no476;
+
+public class NumberComplement_Adv {
+    // 全1异或
+    public int findComplement(int num) {
+        int i = 0;
+        while ((num >> i) != 0) i++;
+        return ((1 << i) - 1) ^ num;
+    }
+}
