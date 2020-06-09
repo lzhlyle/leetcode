859b65afa867dece1c1819845e1dcd803fcd6fc4
lzhlyle <lzhlyle@msn.cn>
2020-06-09 14:33:53 +0800

    week: 106.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no106/ConstructBinaryTreeFromInorderAndPostorderTraversal_DC.java
@@ -0,0 +1,23 @@
+package com.lzhlyle.leetcode.week.no106;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConstructBinaryTreeFromInorderAndPostorderTraversal_DC {
+    // recursion
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        int len = inorder.length;
+        Map<Integer, Integer> map = new HashMap<>(); // (val, inorder's index)
+        for (int i = 0; i < len; i++) map.put(inorder[i], i);
+        return build(map, 0, len - 1, postorder, 0, len - 1);
+    }
+
+    private TreeNode build(Map<Integer, Integer> map, int il, int ir, int[] postorder, int pl, int pr) {
+        if (il > ir) return null;
+        TreeNode res = new TreeNode(postorder[pr]);
+        int p = map.get(res.val), lCnt = p - il;
+        res.left = build(map, il, p - 1, postorder, pl, pl + lCnt - 1);
+        res.right = build(map, p + 1, ir, postorder, pl + lCnt, pr - 1);
+        return res;
+    }
+}
