df597db8444308a35fead1a37543ec8df1a2ff7d
lzhlyle <lzhlyle@msn.cn>
2020-06-09 16:57:26 +0800

    week: 1406.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no1406/StoneGameIii_DP_BottomUp.java
@@ -0,0 +1,16 @@
+package com.lzhlyle.leetcode.week.no1406;
+
+public class StoneGameIii_DP_BottomUp {
+    // dp
+    public String stoneGameIII(int[] stoneValue) {
+        int len = stoneValue.length;
+        int[] dp = new int[len + 2]; // ...?, 0, 0
+        int sum = dp[len - 1] = stoneValue[len - 1];
+        for (int i = len - 2; i >= 0; i--) {
+            sum += stoneValue[i];
+            dp[i] = sum - Math.min(dp[i + 1], Math.min(dp[i + 2], dp[i + 3]));
+        }
+        int a = dp[0], b = sum - a;
+        return a == b ? "Tie" : (a > b ? "Alice" : "Bob");
+    }
+}
