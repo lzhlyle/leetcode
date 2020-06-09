851831a4f2cab324c41928bb3aac8e64669f498e
lzhlyle <lzhlyle@msn.cn>
2020-06-09 14:17:11 +0800

    week: 110.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no110/BalancedBinaryTree.java
@@ -0,0 +1,18 @@
+package com.lzhlyle.leetcode.week.no110;
+
+public class BalancedBinaryTree {
+    private boolean isBalanced;
+
+    public boolean isBalanced(TreeNode root) {
+        isBalanced = true;
+        depth(root);
+        return isBalanced;
+    }
+
+    private int depth(TreeNode curr) {
+        if (curr == null || !isBalanced) return 0;
+        int l = depth(curr.left), r = depth(curr.right);
+        isBalanced = isBalanced && Math.abs(l - r) < 2;
+        return Math.max(l, r) + 1;
+    }
+}
