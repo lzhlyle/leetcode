7e0e5d5d6b3e01f5def89e9964c1686c39ab1685
lzhlyle <lzhlyle@msn.cn>
2020-06-13 18:32:47 +0800

    week: 2.

--- src/main/java/com/lzhlyle/leetcode/week/no2/AddTwoNumbers_Recursion.java
+++ src/main/java/com/lzhlyle/leetcode/week/no2/AddTwoNumbers_Recursion.java
@@ -1,4 +1,4 @@
-package com.lzhlyle.leetcode.tomorrow.no2;
+package com.lzhlyle.leetcode.week.no2;
 
 public class AddTwoNumbers_Recursion {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
