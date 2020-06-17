52c89ae0abf2325cf3ed0b180d2c2a1de0a90360
lzhlyle <lzhlyle@msn.cn>
2020-06-17 19:36:21 +0800

    recite and self: 1300.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no1300/SumOfMutatedArrayClosestToTarget_BS.java
@@ -0,0 +1,28 @@
+package com.lzhlyle.leetcode.self.no1300;
+
+public class SumOfMutatedArrayClosestToTarget_BS {
+    public int findBestValue(int[] arr, int target) {
+        int l = 0, r = arr[0];
+        for (int v : arr)
+            r = Math.max(r, v);
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            // 排除小的，留下大于等于的
+            if (calc(arr, mid) < target) l = mid + 1;
+            else r = mid;
+        }
+
+        int sum1 = calc(arr, l - 1); // 可能小一点
+        int sum2 = calc(arr, l); // 可能大于等于
+        return Math.abs(sum1 - target) <= Math.abs(sum2 - target)
+                ? l - 1 : l;
+    }
+
+    private int calc(int[] arr, int bound) {
+        int sum = 0;
+        for (int v : arr)
+            sum += Math.min(v, bound);
+        return sum;
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/self/no1300/SumOfMutatedArrayClosestToTarget_Greedy_LC.java
@@ -0,0 +1,18 @@
+package com.lzhlyle.leetcode.self.no1300;
+
+import java.util.Arrays;
+
+public class SumOfMutatedArrayClosestToTarget_Greedy_LC {
+    public int findBestValue(int[] arr, int target) {
+        Arrays.sort(arr);
+        int n = arr.length, i = 0;
+        while (i < n && target > arr[i] * (n - i)) {
+            target -= arr[i++];
+        }
+        if (i == n) return arr[n - 1];
+        int res = target / (n - i);
+        if (target - res * (n - i) > (res + 1) * (n - i) - target)
+            res++;
+        return res;
+    }
+}
