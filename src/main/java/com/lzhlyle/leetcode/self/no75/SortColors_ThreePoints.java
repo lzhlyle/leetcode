a0dba9a763f3976ba461b750607b5fb9c350029c
lzhlyle <lzhlyle@msn.cn>
2020-06-20 23:55:15 +0800

    recite and self: 75.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no75/SortColors_ThreePoints.java
@@ -0,0 +1,18 @@
+package com.lzhlyle.leetcode.recite.no75;
+
+public class SortColors_ThreePoints {
+    public void sortColors(int[] nums) {
+        int l = 0, r = nums.length - 1, p = l;
+        while (p <= r) {
+            if (nums[p] == 0) swap(nums, p++, l++);
+            else if (nums[p] == 2) swap(nums, p, r--);
+            else p++;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int swap = nums[i];
+        nums[i] = nums[j];
+        nums[j] = swap;
+    }
+}
