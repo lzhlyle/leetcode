983e480a810e69c8003f28df1afac54c60507fa4
lzhlyle <lzhlyle@msn.cn>
2020-06-20 23:27:45 +0800

    recite and self: 739.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no739/DailyTemperatures_Array.java
@@ -0,0 +1,18 @@
+package com.lzhlyle.leetcode.self.no739;
+
+public class DailyTemperatures_Array {
+    public int[] dailyTemperatures(int[] T) {
+        int n = T.length;
+        if (n < 2) return new int[n];
+
+        int[] res = new int[n], arr = new int[n]; // arr: desc-stack
+        for (int i = 1, ai = 1; i < n; i++) {
+            while (ai > 0 && T[arr[ai - 1]] < T[i]) {
+                int j = arr[ai-- - 1];
+                res[j] = i - j;
+            }
+            arr[ai++] = i;
+        }
+        return res;
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/self/no739/DailyTemperatures_Stack.java
@@ -0,0 +1,22 @@
+package com.lzhlyle.leetcode.self.no739;
+
+import java.util.Stack;
+
+public class DailyTemperatures_Stack {
+    public int[] dailyTemperatures(int[] T) {
+        int n = T.length;
+        if (n < 2) return new int[n];
+
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>(); // index: desc
+        stack.push(0);
+        for (int i = 1; i < n; i++) {
+            while (!stack.isEmpty() && T[stack.peek()] < T[i]) {
+                int j = stack.pop();
+                res[j] = i - j;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
