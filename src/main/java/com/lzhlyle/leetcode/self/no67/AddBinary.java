c945666170fe1c3822f21195c9888b216d69bf3f
lzhlyle <lzhlyle@msn.cn>
2020-06-23 15:37:11 +0800

    recite and self: 67.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no67/AddBinary.java
@@ -0,0 +1,22 @@
+package com.lzhlyle.leetcode.self.no67;
+
+public class AddBinary {
+    public String addBinary(String a, String b) {
+        int alen = a.length(), blen = b.length();
+        if (alen > blen) return addBinary(b, a);
+        // make sure: alen <= blen
+        char[] res = new char[blen + 1];
+        char[] aarr = a.toCharArray(), barr = b.toCharArray();
+        int carry = 0;
+        for (int ri = blen, ai = alen - 1, bi = blen - 1; bi >= 0; ai--, bi--, ri--) {
+            int abit = (ai >= 0 ? aarr[ai] : '0') - '0';
+            int bbit = barr[bi] - '0';
+            int oneCnt = abit + bbit + carry;
+            res[ri] = (char) ((oneCnt & 1) + '0');
+            carry = oneCnt >> 1;
+        }
+        if (carry == 0) return new String(res, 1, blen);
+        res[0] = '1';
+        return new String(res);
+    }
+}
