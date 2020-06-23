66542a98f90497b890652921465e54ad088c177d
lzhlyle <lzhlyle@msn.cn>
2020-06-23 16:34:34 +0800

    recite and self: 371.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no371/SumOfTwoIntegers.java
@@ -0,0 +1,7 @@
+package com.lzhlyle.leetcode.recite.no371;
+
+public class SumOfTwoIntegers {
+    public int getSum(int a, int b) {
+        return (b == 0) ? a : getSum(a ^ b, (a & b) << 1);
+    }
+}
