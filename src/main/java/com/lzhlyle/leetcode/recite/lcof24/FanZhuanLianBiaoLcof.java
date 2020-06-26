3221d820cd39af790a0f8921035d10d130079844
lzhlyle <lzhlyle@msn.cn>
2020-06-26 23:47:39 +0800

    recite and self: lcof-24.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/lcof24/FanZhuanLianBiaoLcof.java
@@ -0,0 +1,16 @@
+package com.lzhlyle.leetcode.recite.lcof24;
+
+public class FanZhuanLianBiaoLcof {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head, prev = null, next;
+        while (curr != null) {
+            next = curr.next;
+            // reverse
+            curr.next = prev;
+            // drill down
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/recite/lcof24/ListNode.java
@@ -0,0 +1,10 @@
+package com.lzhlyle.leetcode.recite.lcof24;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
