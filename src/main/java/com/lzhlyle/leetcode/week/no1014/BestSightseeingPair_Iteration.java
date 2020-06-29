a7d253b41e76f428027fe382ed779a15c525316e
lzhlyle <lzhlyle@msn.cn>
2020-06-29 16:29:25 +0800

    week: 1014.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no1014/BestSightseeingPair_Iteration.java
@@ -0,0 +1,12 @@
+package com.lzhlyle.leetcode.week.no1014;
+
+public class BestSightseeingPair_Iteration {
+    public int maxScoreSightseeingPair(int[] A) {
+        int res = 0, add = A[0] + 0;
+        for (int j = 1; j < A.length; j++) {
+            res = Math.max(res, add + A[j] - j);
+            add = Math.max(add, A[j] + j);
+        }
+        return res;
+    }
+}
