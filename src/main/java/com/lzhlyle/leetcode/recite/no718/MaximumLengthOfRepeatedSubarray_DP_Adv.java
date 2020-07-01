aa5703b734cd4d85646c8f1ef0d0aa84d85b1163
lzhlyle <lzhlyle@msn.cn>
2020-07-01 16:12:54 +0800

    recite and self: 718.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no718/MaximumLengthOfRepeatedSubarray_DP_Adv.java
@@ -0,0 +1,18 @@
+package com.lzhlyle.leetcode.recite.no718;
+
+public class MaximumLengthOfRepeatedSubarray_DP_Adv {
+    public int findLength(int[] A, int[] B) {
+        int m = A.length, n = B.length;
+        int[][] dp = new int[m + 1][n + 1];
+        int max = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (A[i] == B[j]) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                    max = Math.max(max, dp[i + 1][j + 1]);
+                }
+            }
+        }
+        return max;
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/self/no718/MaximumLengthOfRepeatedSubarray_DP.java
@@ -0,0 +1,21 @@
+package com.lzhlyle.leetcode.self.no718;
+
+public class MaximumLengthOfRepeatedSubarray_DP {
+    public int findLength(int[] A, int[] B) {
+        int n = B.length;
+        int[] dp = new int[n + 1];
+        int max = 0;
+        for (int a : A) {
+            int ori = 0;
+            for (int j = 0; j < n; j++) {
+                int next = dp[j + 1];
+                if (a == B[j]) {
+                    dp[j + 1] = ori + 1;
+                    max = Math.max(max, dp[j + 1]);
+                } else dp[j + 1] = 0;
+                ori = next;
+            }
+        }
+        return max;
+    }
+}
