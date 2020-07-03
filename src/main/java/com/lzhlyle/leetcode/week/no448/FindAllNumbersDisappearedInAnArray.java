a36351e37ab6eede6cf05659a441b88e6abc1fe5
lzhlyle <lzhlyle@msn.cn>
2020-07-03 23:38:14 +0800

    week: 448.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no448/FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,20 @@
+package com.lzhlyle.leetcode.week.no448;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllNumbersDisappearedInAnArray {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n = nums.length;
+        List<Integer> res = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) nums[index] = -nums[index];
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) res.add(i + 1);
+            nums[i] = -nums[i];
+        }
+        return res;
+    }
+}
