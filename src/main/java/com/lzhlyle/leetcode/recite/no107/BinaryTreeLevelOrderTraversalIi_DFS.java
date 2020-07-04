22ffef8e334e13cfb3b333eb819e8207965f8b97
lzhlyle <lzhlyle@msn.cn>
2020-07-04 13:03:34 +0800

    recite and self: 107.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no107/BinaryTreeLevelOrderTraversalIi_DFS.java
@@ -0,0 +1,22 @@
+package com.lzhlyle.leetcode.recite.no107;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BinaryTreeLevelOrderTraversalIi_DFS {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(0, root, res);
+        Collections.reverse(res);
+        return res;
+    }
+
+    private void dfs(int level, TreeNode node, List<List<Integer>> res) {
+        if (node == null) return;
+        if (res.size() == level) res.add(new ArrayList<>());
+        dfs(level + 1, node.left, res);
+        dfs(level + 1, node.right, res);
+        res.get(level).add(node.val);
+    }
+}
