ab1f933bfdf9e114f55d69dd75a94a8f8b1db730
lzhlyle <lzhlyle@msn.cn>
2020-07-04 21:07:46 +0800

    recite and self: 112.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no112/PathSum_DFS_LC_Recursion.java
@@ -0,0 +1,11 @@
+package com.lzhlyle.leetcode.recite.no112;
+
+public class PathSum_DFS_LC_Recursion {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null && root.val == sum)
+            return true;
+        return hasPathSum(root.left, sum - root.val)
+                || hasPathSum(root.right, sum - root.val);
+    }
+}
