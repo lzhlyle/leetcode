b5d42ae09595970b48635acc911c6772f7f2cdab
lzhlyle <lzhlyle@msn.cn>
2020-07-04 13:15:07 +0800

    recite and self: 83.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no83/RemoveDuplicatesFromSortedList_Iteration.java
@@ -0,0 +1,17 @@
+package com.lzhlyle.leetcode.self.no83;
+
+public class RemoveDuplicatesFromSortedList_Iteration {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+        ListNode curr = head.next, prev = head;
+        while (curr != null) {
+            if (curr.val == prev.val) {
+                prev.next = curr = curr.next;
+            } else {
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+}
