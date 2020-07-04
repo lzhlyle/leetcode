90d0f7e83ca6d2434f4b15abaf5f9b49465168f5
lzhlyle <lzhlyle@msn.cn>
2020-07-04 13:21:08 +0800

    recite and self: 108.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no108/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,16 @@
+package com.lzhlyle.leetcode.recite.no108;
+
+public class ConvertSortedArrayToBinarySearchTree {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return build(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode build(int[] nums, int l, int r) {
+        if (l > r) return null;
+        int mid = l + (r - l) / 2;
+        TreeNode res = new TreeNode(nums[mid]);
+        res.left = build(nums, l, mid - 1);
+        res.right = build(nums, mid + 1, r);
+        return res;
+    }
+}
