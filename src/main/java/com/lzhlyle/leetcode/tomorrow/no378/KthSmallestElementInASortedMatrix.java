32688a0807d9b53d6d60a61618935c83e3722461
lzhlyle <lzhlyle@msn.cn>
2020-07-05 17:30:42 +0800

    tomorrow: 378.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no378/KthSmallestElementInASortedMatrix.java
@@ -0,0 +1,29 @@
+package com.lzhlyle.leetcode.tomorrow.no378;
+
+public class KthSmallestElementInASortedMatrix {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        if (n == 0) return 0;
+
+        int l = matrix[0][0], r = matrix[n - 1][n - 1];
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            int cnt = lessOrEqualsCnt(matrix, mid);
+            if (cnt < k) l = mid + 1;
+            else r = mid;
+        }
+        return l;
+    }
+
+    private int lessOrEqualsCnt(int[][] matrix, int mid) {
+        int n = matrix.length, cnt = 0;
+        int i = 0, j = n - 1;
+        while (i < n && j >= 0) {
+            if (matrix[i][j] <= mid) {
+                cnt += j + 1;
+                i++;
+            } else j--;
+        }
+        return cnt;
+    }
+}
