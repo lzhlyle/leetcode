fc88389733d7ee88631405fd11c627fe64e65437
lzhlyle <lzhlyle@msn.cn>
2020-07-07 22:07:50 +0800

    recite and self: 837.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no837/New21Game.java
@@ -0,0 +1,15 @@
+package com.lzhlyle.leetcode.recite.no837;
+
+public class New21Game {
+    public double new21Game(int N, int K, int W) {
+        double[] dp = new double[K + W];
+        double sum = 0d;
+        for (int i = K; i <= N && i < K + W; i++)
+            sum += dp[i] = 1d;
+        for (int i = K - 1; i >= 0; i--) {
+            dp[i] = sum / W;
+            sum += dp[i] - dp[i + W];
+        }
+        return dp[0];
+    }
+}
