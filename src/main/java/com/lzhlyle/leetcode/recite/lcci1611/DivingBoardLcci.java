7b7ddfbe96888492f1e703982dcafbe98c515ed4
lzhlyle <lzhlyle@msn.cn>
2020-07-08 07:57:34 +0800

    recite and self: lcci-16.11

---
+++ src/main/java/com/lzhlyle/leetcode/recite/lcci1611/DivingBoardLcci.java
@@ -0,0 +1,16 @@
+package com.lzhlyle.leetcode.recite.lcci1611;
+
+public class DivingBoardLcci {
+    public int[] divingBoard(int s, int l, int k) {
+        if (k == 0) return new int[0];
+        if (s == l) return new int[]{s * k};
+
+        int[] res = new int[k + 1];
+        int i = 0, mul = s * k;
+        while (k-- >= 0) {
+            res[i++] = mul;
+            mul += l - s;
+        }
+        return res;
+    }
+}
