0d55415127e03a3f6676eeace691289fc57133ff
lzhlyle <lzhlyle@msn.cn>
2020-07-07 18:43:50 +0800

    recite and self: 209.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no209/MinimumSizeSubarraySum_SlideWindow.java
@@ -0,0 +1,16 @@
+package com.lzhlyle.leetcode.self.no209;
+
+public class MinimumSizeSubarraySum_SlideWindow {
+    // slide-window: O(n)
+    public int minSubArrayLen(int s, int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for (int r = 0, l = 0, sum = 0; r < nums.length; r++) {
+            sum += nums[r];
+            while (sum >= s) {
+                min = Math.min(min, r - l + 1);
+                sum -= nums[l++];
+            }
+        }
+        return min == Integer.MAX_VALUE ? 0 : min;
+    }
+}
