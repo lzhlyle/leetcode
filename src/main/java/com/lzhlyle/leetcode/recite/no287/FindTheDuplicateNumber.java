ac8de30c8115ec60b576bb25c2d622eb59fa3822
lzhlyle <lzhlyle@msn.cn>
2020-07-08 23:59:49 +0800

    recite and self: 287.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no287/FindTheDuplicateNumber.java
@@ -0,0 +1,18 @@
+package com.lzhlyle.leetcode.recite.no287;
+
+public class FindTheDuplicateNumber {
+    public int findDuplicate(int[] nums) {
+        int fast = 0, slow = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (fast != slow);
+
+        int again = 0;
+        while (again != slow) {
+            again = nums[again];
+            slow = nums[slow];
+        }
+        return again;
+    }
+}
