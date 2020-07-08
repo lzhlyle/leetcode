b9432db1a805d0ceddaa302b09a6693ccbca9306
lzhlyle <lzhlyle@msn.cn>
2020-07-08 23:48:53 +0800

    recite and self: 986.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no986/IntervalListIntersections_MergeSort.java
@@ -0,0 +1,20 @@
+package com.lzhlyle.leetcode.recite.no986;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntervalListIntersections_MergeSort {
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+        List<int[]> list = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < A.length && j < B.length) {
+            int l = Math.max(A[i][0], B[j][0]);
+            int r = Math.min(A[i][1], B[j][1]);
+            if (l <= r) list.add(new int[]{l, r});
+
+            if (r == A[i][1]) i++;
+            else j++;
+        }
+        return list.toArray(new int[list.size()][2]);
+    }
+}
