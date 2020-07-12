71b0f47baee9f32c53d54f49167ba3cd560f859d
lzhlyle <lzhlyle@msn.cn>
2020-07-12 23:32:41 +0800

    recite and self: 174.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no174/DungeonGame_DP.java
@@ -0,0 +1,25 @@
+package com.lzhlyle.leetcode.recite.no174;
+
+public class DungeonGame_DP {
+    public int calculateMinimumHP(int[][] dungeon) {
+        if (dungeon.length == 0 || dungeon[0].length == 0) return 0;
+        int m = dungeon.length, n = dungeon[0].length;
+
+        int[][] dp = new int[m][n];
+        dp[m - 1][n - 1] = Math.max(1, 1 - dungeon[m - 1][n - 1]); // min 1hp left
+
+        for (int i = m - 2; i >= 0; i--)
+            dp[i][n - 1] = Math.max(1, dp[i + 1][n - 1] - dungeon[i][n - 1]);
+        for (int j = n - 2; j >= 0; j--)
+            dp[m - 1][j] = Math.max(1, dp[m - 1][j + 1] - dungeon[m - 1][j]);
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                int min = Math.min(dp[i + 1][j], dp[i][j + 1]);
+                dp[i][j] = Math.max(1, min - dungeon[i][j]);
+            }
+        }
+
+        return dp[0][0];
+    }
+}
