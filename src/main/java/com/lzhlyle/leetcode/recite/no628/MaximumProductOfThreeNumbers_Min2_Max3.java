7b98ef29f8baa817fcff52ab66eb012f376de5ca
lzhlyle <lzhlyle@msn.cn>
2020-07-13 22:48:25 +0800

    recite and self: 628.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no628/MaximumProductOfThreeNumbers_Min2_Max3.java
@@ -0,0 +1,31 @@
+package com.lzhlyle.leetcode.recite.no628;
+
+public class MaximumProductOfThreeNumbers_Min2_Max3 {
+    public int maximumProduct(int[] nums) {
+        int[] arr = new int[5];
+        arr[2] = arr[3] = arr[4] = -1001;
+        for (int v : nums) {
+            if (v < arr[0]) {
+                arr[1] = arr[0];
+                arr[0] = v;
+            } else if (v < arr[1]) {
+                arr[1] = v;
+            }
+
+            if (v > arr[4]) {
+                arr[2] = arr[3];
+                arr[3] = arr[4];
+                arr[4] = v;
+            } else if (v > arr[3]) {
+                arr[2] = arr[3];
+                arr[3] = v;
+            } else if (v > arr[2]) {
+                arr[2] = v;
+            }
+        }
+
+        int a = arr[0], b = arr[1], c = arr[2], d = arr[3], e = arr[4];
+        if (b >= 0) return c * d * e;
+        return Math.max(a * b * e, c * d * e);
+    }
+}
