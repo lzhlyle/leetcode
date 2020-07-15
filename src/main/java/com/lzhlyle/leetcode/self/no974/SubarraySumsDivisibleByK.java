6a9444df4be13a04bdf13699d0daf75ef05bbf4f
lzhlyle <lzhlyle@msn.cn>
2020-07-15 23:39:46 +0800

    recite and self: 974.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no974/SubarraySumsDivisibleByK.java
@@ -0,0 +1,15 @@
+package com.lzhlyle.leetcode.self.no974;
+
+public class SubarraySumsDivisibleByK {
+    public int subarraysDivByK(int[] A, int K) {
+        int[] freq = new int[K]; // freq[i]: count of mod i = (sum / K)
+        freq[0] = 1;
+        int cnt = 0, mod = 0;
+        for (int a : A) {
+            mod = (mod + a) % K;
+            if (mod < 0) mod += K;
+            cnt += freq[mod]++;
+        }
+        return cnt;
+    }
+}
