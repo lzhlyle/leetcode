a74ad97fb218865d19911f104fea739abeeaad8f
lzhlyle <lzhlyle@msn.cn>
2020-07-17 23:41:45 +0800

    recite and self: 451.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no451/SortCharactersByFrequency_Sort.java
@@ -0,0 +1,23 @@
+package com.lzhlyle.leetcode.recite.no451;
+
+import java.util.Arrays;
+
+public class SortCharactersByFrequency_Sort {
+    // O(nlog(n))
+    public String frequencySort(String s) {
+        int[][] freq = new int[256][2];
+        for (char c : s.toCharArray()) {
+            freq[c][0] = c;
+            freq[c][1]++;
+        }
+
+        Arrays.sort(freq, (a, b) -> b[1] - a[1]);
+
+        char[] res = new char[s.length()];
+        int i = 0;
+        for (int[] cf : freq)
+            while (cf[1]-- > 0)
+                res[i++] = (char) cf[0];
+        return new String(res);
+    }
+}
