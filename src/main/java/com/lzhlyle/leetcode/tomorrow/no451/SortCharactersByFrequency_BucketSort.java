18b0d7b5807a4023d486d65851355e4e17cb74da
lzhlyle <lzhlyle@msn.cn>
2020-07-18 12:27:24 +0800

    tomorrow: 451.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no451/SortCharactersByFrequency_BucketSort.java
@@ -0,0 +1,33 @@
+package com.lzhlyle.leetcode.tomorrow.no451;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SortCharactersByFrequency_BucketSort {
+    // O(n)
+    public String frequencySort(String s) {
+        int[] freq = new int[256];
+        for (char c : s.toCharArray()) freq[c]++;
+
+        int n = s.length();
+        List<Integer>[] bucket = new List[n + 1];
+        for (int c = 0; c < 256; c++) {
+            int f = freq[c];
+            if (f == 0) continue;
+            if (bucket[f] == null) bucket[f] = new ArrayList<>();
+            bucket[f].add(c);
+        }
+
+        char[] res = new char[n];
+        for (int f = n, i = 0; f >= 0; f--) {
+            if (bucket[f] != null) {
+                for (int c : bucket[f]) {
+                    int curr = f;
+                    while (curr-- > 0)
+                        res[i++] = (char) c;
+                }
+            }
+        }
+        return new String(res);
+    }
+}
