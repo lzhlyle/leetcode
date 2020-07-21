f1cd593555ab930b81d485d53c5966480e4136a4
lzhlyle <lzhlyle@msn.cn>
2020-07-21 18:30:29 +0800

    week: 628.

--- src/main/java/com/lzhlyle/leetcode/week/no350/IntersectionOfTwoArraysIi_Map_Freq_LC.java
+++ src/main/java/com/lzhlyle/leetcode/week/no350/IntersectionOfTwoArraysIi_Map_Freq_LC.java
@@ -1,4 +1,4 @@
-package com.lzhlyle.leetcode.tomorrow.no350;
+package com.lzhlyle.leetcode.week.no350;
 
 import java.util.Arrays;
 import java.util.HashMap;
---
+++ src/main/java/com/lzhlyle/leetcode/week/no628/MaximumProductOfThreeNumbers_Min2_Max3.java
@@ -0,0 +1,26 @@
+package com.lzhlyle.leetcode.week.no628;
+
+public class MaximumProductOfThreeNumbers_Min2_Max3 {
+    public int maximumProduct(int[] nums) {
+        int min1, min2, max3, max2, max1;
+        min1 = min2 = 1001;
+        max3 = max2 = max1 = -1001;
+        for (int v : nums) {
+            if (v < min1) {
+                min2 = min1;
+                min1 = v;
+            } else if (v < min2) min2 = v;
+
+            if (v > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = v;
+            } else if (v > max2) {
+                max3 = max2;
+                max2 = v;
+            } else if (v > max3) max3 = v;
+        }
+        if (min2 >= 0) return max3 * max2 * max1;
+        return Math.max(min1 * min2 * max1, max3 * max2 * max1);
+    }
+}
