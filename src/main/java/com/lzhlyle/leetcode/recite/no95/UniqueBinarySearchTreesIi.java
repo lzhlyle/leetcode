2460f154f6cf3b832c7360929bbf7efc9e44eb29
lzhlyle <lzhlyle@msn.cn>
2020-07-21 10:34:03 +0800

    recite and self: 95.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no95/UniqueBinarySearchTreesIi.java
@@ -0,0 +1,22 @@
+package com.lzhlyle.leetcode.recite.no95;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class UniqueBinarySearchTreesIi {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) return Collections.emptyList();
+        return build(1, n);
+    }
+
+    private List<TreeNode> build(int l, int r) {
+        if (l > r) return Collections.singletonList(null);
+        List<TreeNode> list = new ArrayList<>();
+        for (int i = l; i <= r; i++)
+            for (TreeNode ln : build(l, i - 1))
+                for (TreeNode rn : build(i + 1, r))
+                    list.add(new TreeNode(i, ln, rn));
+        return list;
+    }
+}
