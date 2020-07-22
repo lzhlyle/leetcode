29549efa2fc46f4be4dfd46e07fa8237ee419bec
lzhlyle <lzhlyle@msn.cn>
2020-07-22 23:09:58 +0800

    recite and self: 298.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no298/BinaryTreeLongestConsecutiveSequence.java
@@ -0,0 +1,25 @@
+package com.lzhlyle.leetcode.self.no298;
+
+public class BinaryTreeLongestConsecutiveSequence {
+    private int max;
+
+    public int longestConsecutive(TreeNode root) {
+        if (root == null) return 0;
+        max = 1;
+        dfs(root);
+        return max;
+    }
+
+    // 从此往下有多长可顺
+    private int dfs(TreeNode node) {
+        if (node == null) return 0;
+        int l = dfs(node.left), r = dfs(node.right);
+        if (l == 0 && r == 0) return 1;
+        if (l > 0 && node.left.val != node.val + 1) l = 0;
+        if (r > 0 && node.right.val != node.val + 1) r = 0;
+
+        int res = Math.max(l, r) + 1;
+        max = Math.max(max, res);
+        return res;
+    }
+}
