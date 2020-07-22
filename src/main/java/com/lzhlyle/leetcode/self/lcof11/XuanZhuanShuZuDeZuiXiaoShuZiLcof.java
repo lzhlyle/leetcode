342f3bf600699bc14a9f2526dae0f119ecadc2d8
lzhlyle <lzhlyle@msn.cn>
2020-07-22 11:32:05 +0800

    recite and self: lcof-11.

---
+++ src/main/java/com/lzhlyle/leetcode/self/lcof11/XuanZhuanShuZuDeZuiXiaoShuZiLcof.java
@@ -0,0 +1,14 @@
+package com.lzhlyle.leetcode.self.lcof11;
+
+public class XuanZhuanShuZuDeZuiXiaoShuZiLcof {
+    public int minArray(int[] arr) {
+        int l = 0, r = arr.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] > arr[r]) l = mid + 1;
+            else if (arr[mid] < arr[l]) r = mid;
+            else r--;
+        }
+        return arr[l];
+    }
+}
