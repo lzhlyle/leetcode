9a095739b2ac4f90318e69c6598e0ac9b05df0e1
lzhlyle <lzhlyle@msn.cn>
2020-07-24 11:39:36 +0800

    recite and self: 1025.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no1025/DivisorGame_DP.java
@@ -0,0 +1,13 @@
+package com.lzhlyle.leetcode.recite.no1025;
+
+public class DivisorGame_DP {
+    public boolean divisorGame(int n) {
+        if (n == 1) return false;
+        boolean[] dp = new boolean[n + 1];
+        dp[2] = true;
+        for (int i = 3; i <= n; i++)
+            for (int j = 1; j < i && !dp[i]; j++)
+                if (i % j == 0 && !dp[i - j]) dp[i] = true;
+        return dp[n];
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/self/no1025/DivisorGame_Math.java
@@ -0,0 +1,7 @@
+package com.lzhlyle.leetcode.self.no1025;
+
+public class DivisorGame_Math {
+    public boolean divisorGame(int n) {
+        return (n & 1) == 0;
+    }
+}
