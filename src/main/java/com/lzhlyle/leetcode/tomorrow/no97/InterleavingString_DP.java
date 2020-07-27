00afe5bcb4b4dce66dc3c7a3d4c0212f58deb5af
lzhlyle <lzhlyle@msn.cn>
2020-07-27 14:10:31 +0800

    tomorrow: 97.

--- src/main/java/com/lzhlyle/leetcode/tomorrow/no97/InterleavingString_DP.java
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no97/InterleavingString_DP.java
@@ -2,23 +2,22 @@ package com.lzhlyle.leetcode.tomorrow.no97;
 
 public class InterleavingString_DP {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1.length() + s2.length() != s3.length()) return false;
+        char[] marr = s1.toCharArray(), narr = s2.toCharArray(), arr = s3.toCharArray();
+        int m = marr.length, n = narr.length, len = arr.length;
+        if (m + n != len) return false;
 
-        char[] arr1 = s1.toCharArray(), arr2 = s2.toCharArray(), arr3 = s3.toCharArray();
-        int m = arr1.length, n = arr2.length;
         boolean[][] dp = new boolean[m + 1][n + 1];
-
         dp[0][0] = true;
-        for (int i = 1; i <= m; i++)
-            if (!(dp[i][0] = arr1[i - 1] == arr3[i - 1])) break;
-        for (int j = 1; j <= n; j++)
-            if (!(dp[0][j] = arr2[j - 1] == arr3[j - 1])) break;
+        for (int i = 1; i <= m && dp[i - 1][0]; i++)
+            dp[i][0] = marr[i - 1] == arr[i - 1];
+        for (int j = 1; j <= n && dp[0][j - 1]; j++)
+            dp[0][j] = narr[j - 1] == arr[j - 1];
 
         for (int i = 1; i <= m; i++) {
-            boolean any = dp[i][0];
+            boolean any = dp[i][0]; // default
             for (int j = 1; j <= n; j++) {
-                dp[i][j] = (arr1[i - 1] == arr3[i + j - 1] && dp[i - 1][j]) ||
-                        (arr2[j - 1] == arr3[i + j - 1] && dp[i][j - 1]);
+                dp[i][j] = (dp[i - 1][j] && marr[i - 1] == arr[i + j - 1]) ||
+                        (dp[i][j - 1] && narr[j - 1] == arr[i + j - 1]);
                 if (!any) any = dp[i][j];
             }
             if (!any) return false;
