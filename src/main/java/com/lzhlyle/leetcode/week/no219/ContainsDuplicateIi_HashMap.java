b2b8e88e697aceadbc7ea0d606b47cb1960557fc
lzhlyle <lzhlyle@msn.cn>
2020-07-30 20:39:22 +0800

    week: 219.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no219/ContainsDuplicateIi_HashMap.java
@@ -0,0 +1,16 @@
+package com.lzhlyle.leetcode.week.no219;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicateIi_HashMap {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) map.put(nums[i], i);
+            else if (i - map.get(nums[i]) <= k) return true;
+            else map.put(nums[i], i);
+        }
+        return false;
+    }
+}
