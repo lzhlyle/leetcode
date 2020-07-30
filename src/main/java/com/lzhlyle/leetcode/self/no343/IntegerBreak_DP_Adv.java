3bf630146dd83e4966d36d1237300490db538f19
lzhlyle <lzhlyle@msn.cn>
2020-07-30 10:02:16 +0800

    recite and self: 343.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no343/IntegerBreak_DP_Adv.java
@@ -0,0 +1,13 @@
+package com.lzhlyle.leetcode.self.no343;
+
+public class IntegerBreak_DP_Adv {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++)
+            dp[i] = Math.max(Math.max(dp[i - 1], i - 1),
+                    Math.max(2 * Math.max(dp[i - 2], i - 2),
+                            3 * Math.max(dp[i - 3], i - 3)));
+        return dp[n];
+    }
+}
