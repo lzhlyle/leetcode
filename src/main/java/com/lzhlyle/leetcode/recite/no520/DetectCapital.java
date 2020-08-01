72628b4e8f3a3b4f230eb187afe1100a9aa4eb6c
lzhlyle <lzhlyle@msn.cn>
2020-08-01 21:07:49 +0800

    recite and self: 520.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no520/DetectCapital.java
@@ -0,0 +1,20 @@
+package com.lzhlyle.leetcode.recite.no520;
+
+public class DetectCapital {
+    public boolean detectCapitalUse(String word) {
+        char[] arr = word.toCharArray();
+        boolean first = cap(arr[0]), second = false;
+        for (int i = 1; i < arr.length; i++) {
+            boolean cap = cap(arr[i]);
+            if (i == 1) {
+                if (!first && cap) return false;
+                second = cap;
+            } else if (second != cap) return false;
+        }
+        return true;
+    }
+
+    private boolean cap(char c) {
+        return c >= 'A' && c <= 'Z';
+    }
+}
