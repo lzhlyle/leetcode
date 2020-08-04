8ecb3fbee1761f64a14a44b15bc60cc11c7a2e6f
lzhlyle <lzhlyle@msn.cn>
2020-08-04 17:36:24 +0800

    week: 103.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no103/BinaryTreeZigzagLevelOrderTraversal_DFS.java
@@ -0,0 +1,24 @@
+package com.lzhlyle.leetcode.week.no103;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BinaryTreeZigzagLevelOrderTraversal_DFS {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if (root == null) return Collections.emptyList();
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(0, root, res);
+        for (int i = 1; i < res.size(); i += 2)
+            Collections.reverse(res.get(i));
+        return res;
+    }
+
+    private void dfs(int level, TreeNode node, List<List<Integer>> res) {
+        if (node == null) return;
+        if (level == res.size()) res.add(new ArrayList<>());
+        res.get(level).add(node.val);
+        dfs(level + 1, node.left, res);
+        dfs(level + 1, node.right, res);
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/week/no103/TreeNode.java
@@ -0,0 +1,11 @@
+package com.lzhlyle.leetcode.week.no103;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
