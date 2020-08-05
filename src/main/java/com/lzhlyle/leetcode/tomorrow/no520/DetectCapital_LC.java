1eb3ac5acfa4d0b0712daeb7fd94aa7e81ba271e
lzhlyle <lzhlyle@msn.cn>
2020-08-05 15:07:54 +0800

    tomorrow: 520.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no520/DetectCapital_LC.java
@@ -0,0 +1,9 @@
+package com.lzhlyle.leetcode.tomorrow.no520;
+
+public class DetectCapital_LC {
+    public boolean detectCapitalUse(String word) {
+        int cnt = 0;
+        for (char c : word.toCharArray()) if (c <= 'Z') cnt++;
+        return cnt == 0 || cnt == word.length() || (cnt == 1 && word.charAt(0) <= 'Z');
+    }
+}
