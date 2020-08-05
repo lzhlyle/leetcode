21c318ca6199735dc8a4750f3623a0f500e7f0e7
lzhlyle <lzhlyle@msn.cn>
2020-08-05 15:04:47 +0800

    tomorrow: 632.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no632/SmallestRangeCoveringElementsFromKLists_SlideWindow.java
@@ -0,0 +1,34 @@
+package com.lzhlyle.leetcode.tomorrow.no632;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class SmallestRangeCoveringElementsFromKLists_SlideWindow {
+    public int[] smallestRange(List<List<Integer>> nums) {
+        int n = nums.size();
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            for (int val : nums.get(i))
+                list.add(new int[]{val, i}); // {val, group}
+        int[][] arr = list.toArray(new int[list.size()][2]);
+        Arrays.sort(arr, Comparator.comparingInt(a -> a[0]));
+
+        int[] freq = new int[n], res = new int[2];
+        int cnt = 0, min = Integer.MAX_VALUE;
+        for (int r = 0, l = 0; r < arr.length; r++) {
+            if (++freq[arr[r][1]] == 1) cnt++;
+            while (cnt == n) {
+                int ll = arr[l][0], rr = arr[r][0];
+                if (rr - ll < min) {
+                    min = rr - ll;
+                    res[0] = ll;
+                    res[1] = rr;
+                }
+                if (--freq[arr[l++][1]] == 0) cnt--;
+            }
+        }
+        return res;
+    }
+}
