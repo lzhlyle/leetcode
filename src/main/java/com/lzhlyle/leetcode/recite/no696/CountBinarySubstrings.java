0b8332eeb1fe4af18a7c669bb1d7841ff86e05a8
lzhlyle <lzhlyle@msn.cn>
2020-08-10 18:19:34 +0800

    recite and self: 696.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no696/CountBinarySubstrings.java
@@ -0,0 +1,20 @@
+package com.lzhlyle.leetcode.recite.no696;
+
+public class CountBinarySubstrings {
+    public int countBinarySubstrings(String s) {
+        if (s.length() == 1) return 0;
+        char[] arr = s.toCharArray();
+        int cnt = 0, a = 1, b = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1]) {
+                a++;
+            } else {
+                cnt += Math.min(a, b);
+                b = a;
+                a = 1;
+            }
+        }
+        cnt += Math.min(a, b);
+        return cnt;
+    }
+}
