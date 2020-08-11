166d71fcb8714570a5b635cba4f268aa12f28495
lzhlyle <lzhlyle@msn.cn>
2020-08-11 18:48:11 +0800

    recite and self: 274.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no274/HIndex_BucketSort.java
@@ -0,0 +1,17 @@
+package com.lzhlyle.leetcode.self.no274;
+
+public class HIndex_BucketSort {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] bucket = new int[n + 1];
+        for (int c : citations) {
+            if (c >= n) bucket[n]++;
+            else bucket[c]++;
+        }
+
+        for (int i = n, sum = 0; i >= 0; i--)
+            if ((sum += bucket[i]) >= i)
+                return i;
+        return 0;
+    }
+}
