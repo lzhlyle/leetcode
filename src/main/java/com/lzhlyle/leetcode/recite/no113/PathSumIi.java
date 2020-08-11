63ca2cd022eb42763f0a9fa14ec8a8345bf8e049
lzhlyle <lzhlyle@msn.cn>
2020-08-11 10:43:50 +0800

    recite and self: 113.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no113/PathSumIi.java
@@ -0,0 +1,26 @@
+package com.lzhlyle.leetcode.recite.no113;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathSumIi {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        if (root == null) return new ArrayList<>();
+
+        List<List<Integer>> res = new ArrayList<>();
+        if (root.left == null && root.right == null) {
+            if (root.val == sum) {
+                List<Integer> list = new ArrayList<>();
+                list.add(root.val);
+                res.add(list);
+                return res;
+            }
+        }
+
+        res.addAll(pathSum(root.left, sum - root.val));
+        res.addAll(pathSum(root.right, sum - root.val));
+        for (List<Integer> list : res)
+            list.add(0, root.val);
+        return res;
+    }
+}
