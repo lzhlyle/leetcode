8221cb271c5eade07810990ffe9b8900f0ec06a1
lzhlyle <lzhlyle@msn.cn>
2020-08-11 22:24:23 +0800

    recite and self: 437.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no437/PathSumIii_Dfs.java
@@ -0,0 +1,20 @@
+package com.lzhlyle.leetcode.recite.no437;
+
+public class PathSumIii_Dfs {
+    public int pathSum(TreeNode root, int sum) {
+        return dfs(root, sum, new int[0]);
+    }
+
+    private int dfs(TreeNode node, int sum, int[] arr) {
+        if (node == null) return 0;
+        int cnt = 0;
+        int[] next = new int[arr.length + 1];
+        int i = 0;
+        for (int v : arr)
+            if ((next[i++] = v + node.val) == sum) cnt++;
+        if ((next[i] = node.val) == sum) cnt++;
+        cnt += dfs(node.left, sum, next);
+        cnt += dfs(node.right, sum, next);
+        return cnt;
+    }
+}
