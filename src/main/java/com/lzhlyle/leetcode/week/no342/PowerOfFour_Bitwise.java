a3a83281ca284d00727745e34b86fd3c9a69a5d5
lzhlyle <lzhlyle@msn.cn>
2020-08-13 19:47:34 +0800

    week: 342.

---
+++ src/main/java/com/lzhlyle/leetcode/week/no342/PowerOfFour_Bitwise.java
@@ -0,0 +1,11 @@
+package com.lzhlyle.leetcode.week.no342;
+
+public class PowerOfFour_Bitwise {
+    public boolean isPowerOfFour(int num) {
+        if (num == 0) return false;
+        if ((num & (num - 1)) != 0) return false;
+        int cnt = 0;
+        while ((num >>> cnt) != 1) cnt++;
+        return (cnt & 1) == 0;
+    }
+}
