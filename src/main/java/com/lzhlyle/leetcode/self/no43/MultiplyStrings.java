347662929c481c88e65d409fe82ab5324654c0c1
lzhlyle <lzhlyle@msn.cn>
2020-08-13 19:40:15 +0800

    recite and self: 43.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no43/MultiplyStrings.java
@@ -0,0 +1,52 @@
+package com.lzhlyle.leetcode.self.no43;
+
+public class MultiplyStrings {
+    public String multiply(String num1, String num2) {
+        if (num1.isEmpty() || num2.isEmpty()) return "";
+        if (num1.equals("0") || num2.equals("0")) return "0";
+        if (num1.length() > num2.length()) return multiply(num2, num1);
+
+        String res = "0";
+        char[] arr1 = num1.toCharArray(), arr2 = num2.toCharArray();
+        for (int i = arr1.length - 1, zero = 0; i >= 0; i--, zero++) {
+            int a = arr1[i] - '0';
+            String mul = mul(arr2, a, zero);
+            res = add(res, mul);
+        }
+        return res;
+    }
+
+    private String mul(char[] arr, int num, int zero) {
+        if (num == 0) return "0";
+        StringBuilder builder = new StringBuilder();
+        int carry = 0;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int tot = (arr[i] - '0') * num + carry;
+            carry = tot / 10;
+            builder.append(tot % 10);
+        }
+        if (carry > 0) builder.append(carry);
+        builder.reverse();
+        while (zero-- > 0) builder.append('0');
+        return builder.toString();
+    }
+
+    private String add(String a, String b) {
+        if (a.length() > b.length()) return add(b, a);
+        char[] aarr = a.toCharArray(), barr = b.toCharArray();
+        StringBuilder builder = new StringBuilder();
+        int carry = 0, j = barr.length - 1;
+        for (int i = aarr.length - 1; i >= 0; i--, j--) {
+            int tot = (aarr[i] - '0') + (barr[j] - '0') + carry;
+            carry = tot / 10;
+            builder.append(tot % 10);
+        }
+        while (j >= 0) {
+            int tot = (barr[j--] - '0') + carry;
+            carry = tot / 10;
+            builder.append(tot % 10);
+        }
+        if (carry > 0) builder.append(carry);
+        return builder.reverse().toString();
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/self/no43/MultiplyStrings_LC.java
@@ -0,0 +1,26 @@
+package com.lzhlyle.leetcode.self.no43;
+
+public class MultiplyStrings_LC {
+    public String multiply(String num1, String num2) {
+        if (num1.isEmpty() || num2.isEmpty()) return "";
+        if (num1.equals("0") || num2.equals("0")) return "0";
+
+        char[] arr1 = num1.toCharArray(), arr2 = num2.toCharArray();
+        int[] arr = new int[arr1.length + arr2.length];
+        for (int i = arr1.length - 1; i >= 0; i--) {
+            int a = arr1[i] - '0';
+            for (int j = arr2.length - 1; j >= 0; j--) {
+                int b = arr2[j] - '0';
+                int sum = arr[i + j + 1] + a * b;
+                arr[i + j + 1] = sum % 10;
+                arr[i + j] += sum / 10;
+            }
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < arr.length; i++)
+            if (i > 0 || arr[i] > 0)
+                builder.append(arr[i]);
+        return builder.toString();
+    }
+}
