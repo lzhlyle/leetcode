23f5fcebe3fba610df165419f5f3196b4eea5b07
lzhlyle <lzhlyle@msn.cn>
2020-08-19 23:57:47 +0800

    tomorrow: 109.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no109/ConvertSortedListToBinarySearchTree_FastSlow.java
@@ -0,0 +1,17 @@
+package com.lzhlyle.leetcode.tomorrow.no109;
+
+public class ConvertSortedListToBinarySearchTree_FastSlow {
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) return null;
+        if (head.next == null) return new TreeNode(head.val);
+        ListNode fast = head, slow = head, prev = null;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            prev = slow;
+            slow = slow.next;
+        }
+        ListNode l = head, r = slow.next;
+        prev.next = slow.next = null; // disconnect: l...prev, slow, r...fast
+        return new TreeNode(slow.val, sortedListToBST(l), sortedListToBST(r));
+    }
+}
