eb312d0bfadd3f5fa9ed815d8aa2c07532c63fe5
lzhlyle <lzhlyle@msn.cn>
2020-08-20 17:58:27 +0800

    recite and self: 824.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no824/GoatLatin.java
@@ -0,0 +1,25 @@
+package com.lzhlyle.leetcode.self.no824;
+
+public class GoatLatin {
+    public String toGoatLatin(String S) {
+        String[] arr = S.split(" ");
+        StringBuilder suffix = new StringBuilder("a");
+        for (int i = 0; i < arr.length; i++) {
+            String ori = arr[i];
+            if (!valid(ori.charAt(0)))
+                arr[i] = ori.substring(1) + ori.charAt(0);
+            arr[i] += "ma" + suffix;
+            suffix.append("a");
+        }
+        return String.join(" ", arr);
+    }
+
+    private boolean valid(char c) {
+        char lower = Character.toLowerCase(c);
+        return lower == 'a' ||
+                lower == 'e' ||
+                lower == 'i' ||
+                lower == 'o' ||
+                lower == 'u';
+    }
+}
