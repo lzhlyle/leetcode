e32c1583120cf244dc3f3aff5ee19719fac5ff28
lzhlyle <lzhlyle@msn.cn>
2020-08-22 18:28:14 +0800

    tomorrow: 113.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no113/TreeNode.java
@@ -0,0 +1,12 @@
+package com.lzhlyle.leetcode.tomorrow.no113;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+}
