fd9c402d87daad3407845dd7672833c91e53bf29
lzhlyle <lzhlyle@msn.cn>
2020-08-25 11:15:33 +0800

    tomorrow: 119.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no119/PascalsTriangleIi.java
@@ -0,0 +1,16 @@
+package com.lzhlyle.leetcode.tomorrow.no119;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PascalsTriangleIi {
+    public List<Integer> getRow(int n) {
+        Integer[] arr = new Integer[n + 1];
+        Arrays.fill(arr, 0);
+        arr[0] = 1;
+        for (int r = 1; r <= n; r++)
+            for (int i = r; i > 0; i--)
+                arr[i] += arr[i - 1];
+        return Arrays.asList(arr);
+    }
+}
