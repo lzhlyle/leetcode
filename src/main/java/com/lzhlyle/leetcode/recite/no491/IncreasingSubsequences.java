e4d411289b3f6243f169e201f982c0e09560d1c1
lzhlyle <lzhlyle@msn.cn>
2020-08-25 23:53:28 +0800

    recite and self: 491.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no491/IncreasingSubsequences.java
@@ -0,0 +1,25 @@
+package com.lzhlyle.leetcode.recite.no491;
+
+import java.util.*;
+
+public class IncreasingSubsequences {
+    public List<List<Integer>> findSubsequences(int[] arr) {
+        List<List<Integer>> res = new ArrayList<>();
+        bt(arr, 0, new Stack<>(), res);
+        return res;
+    }
+
+    private void bt(int[] arr, int i, Stack<Integer> curr, List<List<Integer>> res) {
+        if (curr.size() > 1) res.add(new ArrayList<>(curr));
+        Set<Integer> set = new HashSet<>();
+        for (int j = i; j < arr.length; j++) {
+            if (set.add(arr[j])) {
+                if (curr.isEmpty() || arr[j] >= curr.peek()) {
+                    curr.push(arr[j]);
+                    bt(arr, j + 1, curr, res);
+                    curr.pop();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/self/no491/IncreasingSubsequences_LC.java
@@ -0,0 +1,30 @@
+package com.lzhlyle.leetcode.self.no491;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class IncreasingSubsequences_LC {
+    public List<List<Integer>> findSubsequences(int[] arr) {
+        List<List<Integer>> res = new ArrayList<>();
+        bt(0, arr, new Stack<>(), res);
+        return res;
+    }
+
+    public void bt(int i, int[] arr, Stack<Integer> stack, List<List<Integer>> res) {
+        if (i == arr.length) {
+            if (stack.size() > 1)
+                res.add(new ArrayList<>(stack));
+            return;
+        }
+
+        if (stack.isEmpty() || arr[i] >= stack.peek()) {
+            stack.push(arr[i]);
+            bt(i + 1, arr, stack, res);
+            stack.pop();
+        }
+
+        if (stack.isEmpty() || arr[i] != stack.peek())
+            bt(i + 1, arr, stack, res);
+    }
+}
