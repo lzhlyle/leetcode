e004067d7603daeeb9aab79dbc2a2717290e8ea6
lzhlyle <lzhlyle@msn.cn>
2020-08-25 10:25:44 +0800

    tomorrow: 905.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no905/SortArrayByParity.java
@@ -0,0 +1,17 @@
+package com.lzhlyle.leetcode.tomorrow.no905;
+
+public class SortArrayByParity {
+    public int[] sortArrayByParity(int[] arr) {
+        int l = 0, r = arr.length - 1;
+        while (l < r) {
+            while (l < r && (arr[l] & 1) == 0) l++;
+            while (l < r && (arr[r] & 1) == 1) r--;
+            if (l < r) {
+                int swap = arr[l];
+                arr[l++] = arr[r];
+                arr[r--] = swap;
+            }
+        }
+        return arr;
+    }
+}
