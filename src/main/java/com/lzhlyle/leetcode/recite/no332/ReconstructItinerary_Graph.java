36436ddc31118e7a069ba074ee1eb0c02e19920a
lzhlyle <lzhlyle@msn.cn>
2020-08-28 00:06:14 +0800

    recite and self: 332.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no332/ReconstructItinerary_Graph.java
@@ -0,0 +1,28 @@
+package com.lzhlyle.leetcode.recite.no332;
+
+import java.util.*;
+
+public class ReconstructItinerary_Graph {
+    public List<String> findItinerary(List<List<String>> tickets) {
+        if (tickets.isEmpty()) return Collections.emptyList();
+
+        Map<String, Queue<String>> map = new HashMap<>();
+        for (List<String> t : tickets) {
+            String from = t.get(0), to = t.get(1);
+            if (!map.containsKey(from))
+                map.put(from, new PriorityQueue<>());
+            map.get(from).add(to);
+        }
+
+        List<String> res = new LinkedList<>();
+        visit("JFK", map, res);
+        return res;
+    }
+
+    private void visit(String from, Map<String, Queue<String>> map, List<String> res) {
+        Queue<String> tos = map.get(from);
+        while (tos != null && !tos.isEmpty())
+            visit(tos.remove(), map, res);
+        res.add(0, from);
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/self/no332/ReconstructItinerary_BT_Modeling.java
@@ -0,0 +1,57 @@
+package com.lzhlyle.leetcode.self.no332;
+
+import java.util.*;
+
+public class ReconstructItinerary_BT_Modeling {
+    // bt
+    public List<String> findItinerary(List<List<String>> tickets) {
+        if (tickets.isEmpty()) return Collections.emptyList();
+
+        Map<String, Map<String, Integer>> iniMap = new HashMap<>(); // (from, tos)
+        for (List<String> t : tickets) {
+            String from = t.get(0), to = t.get(1);
+            if (!iniMap.containsKey(from))
+                iniMap.put(from, new HashMap<>());
+            iniMap.get(from).put(to, iniMap.get(from).getOrDefault(to, 0) + 1);
+        }
+
+        Map<String, List<ToLocation>> map = new HashMap<>();
+        for (String from : iniMap.keySet()) {
+            List<ToLocation> list = new ArrayList<>();
+            for (String to : iniMap.get(from).keySet())
+                list.add(new ToLocation(to, iniMap.get(from).get(to)));
+            list.sort(Comparator.comparing(a -> a.name));
+            map.put(from, list);
+        }
+
+        Stack<String> path = new Stack<>();
+        path.push("JFK");
+        bt(0, tickets.size(), path, map);
+        return new ArrayList<>(path);
+    }
+
+    private boolean bt(int level, int n, Stack<String> path, Map<String, List<ToLocation>> map) {
+        if (level == n) return true;
+        if (!map.containsKey(path.peek())) return false;
+
+        for (ToLocation to : map.get(path.peek())) {
+            if (to.cnt == 0) continue;
+            path.push(to.name);
+            to.cnt--;
+            if (bt(level + 1, n, path, map)) return true;
+            to.cnt++;
+            path.pop();
+        }
+        return false;
+    }
+
+    class ToLocation {
+        String name;
+        int cnt;
+
+        ToLocation(String name, int cnt) {
+            this.name = name;
+            this.cnt = cnt;
+        }
+    }
+}
