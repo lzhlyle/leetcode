4a09f9022d2d161e4a22b338e536ea2c61f79ec7
lzhlyle <lzhlyle@msn.cn>
2020-08-29 23:50:02 +0800

    tomorrow: 404.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no404/SumOfLeftLeaves_Iteration.java
@@ -0,0 +1,22 @@
+package com.lzhlyle.leetcode.self.no404;
+
+import java.util.Stack;
+
+public class SumOfLeftLeaves_Iteration {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        int res = 0;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node.left != null) {
+                if (node.left.left == null && node.left.right == null)
+                    res += node.left.val;
+                else stack.push(node.left);
+            }
+            if (node.right != null) stack.push(node.right);
+        }
+        return res;
+    }
+}
