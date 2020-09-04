46454cd5c6f45602c27b06d0a0d818d9d9365035
lzhlyle <lzhlyle@msn.cn>
2020-09-04 21:35:43 +0800

    recite and self: 257.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no257/BinaryTreePaths_BT.java
@@ -0,0 +1,28 @@
+package com.lzhlyle.leetcode.self.no257;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTreePaths_BT {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        bt(root, new Stack<>(), res);
+        return res;
+    }
+
+    private void bt(TreeNode node, Stack<Integer> stack, List<String> res) {
+        if (node == null) return;
+        stack.push(node.val);
+        if (node.left == null && node.right == null) {
+            StringBuilder builder = new StringBuilder();
+            for (Integer v : stack)
+                builder.append(v).append("->");
+            res.add(builder.substring(0, builder.length() - 2));
+        } else {
+            bt(node.left, stack, res);
+            bt(node.right, stack, res);
+        }
+        stack.pop();
+    }
+}
