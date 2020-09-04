d6044fe84a70b1e84531304ab7bdbc9d8566e532
lzhlyle <lzhlyle@msn.cn>
2020-09-04 22:02:37 +0800

    recite and self: 687.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no687/LongestUnivaluePath_LC.java
@@ -0,0 +1,20 @@
+package com.lzhlyle.leetcode.self.no687;
+
+public class LongestUnivaluePath_LC {
+    private int max;
+
+    public int longestUnivaluePath(TreeNode root) {
+        if (root == null) return 0;
+        max = 0;
+        dfs(root, root.val);
+        return max;
+    }
+
+    private int dfs(TreeNode node, int last) {
+        if (node == null) return 0;
+        int l = dfs(node.left, node.val), r = dfs(node.right, node.val);
+        max = Math.max(max, l + r);
+        if (node.val == last) return Math.max(l, r) + 1;
+        return 0;
+    }
+}
