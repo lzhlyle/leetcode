c214cd3c54aab93ed409b2cb6c59ae729d9c6a2e
lzhlyle <lzhlyle@msn.cn>
2020-09-05 15:51:48 +0800

    recite and self: 1305.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no1305/AllElementsInTwoBinarySearchTrees.java
@@ -0,0 +1,28 @@
+package com.lzhlyle.leetcode.self.no1305;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllElementsInTwoBinarySearchTrees {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = new ArrayList<>(), list2 = new ArrayList<>();
+        dfs(root1, list1);
+        dfs(root2, list2);
+        List<Integer> res = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < list1.size() && j < list2.size()) {
+            if (list1.get(i) < list2.get(j)) res.add(list1.get(i++));
+            else res.add(list2.get(j++));
+        }
+        while (i < list1.size()) res.add(list1.get(i++));
+        while (j < list2.size()) res.add(list2.get(j++));
+        return res;
+    }
+
+    private void dfs(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        dfs(node.left, list);
+        list.add(node.val);
+        dfs(node.right, list);
+    }
+}
