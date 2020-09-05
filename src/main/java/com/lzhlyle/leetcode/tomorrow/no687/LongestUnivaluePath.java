b055353d09f9296fff472d70e48952bd76d1b7d9
lzhlyle <lzhlyle@msn.cn>
2020-09-05 16:25:03 +0800

    tomorrow: 687.

---
+++ src/main/java/com/lzhlyle/leetcode/tomorrow/no687/LongestUnivaluePath.java
@@ -0,0 +1,23 @@
+package com.lzhlyle.leetcode.tomorrow.no687;
+
+public class LongestUnivaluePath {
+    private int max;
+
+    public int longestUnivaluePath(TreeNode root) {
+        max = 0;
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) return 0;
+        int l = dfs(node.left), r = dfs(node.right);
+        int total = 1;
+        if (l > 0 && node.val == node.left.val) total += l;
+        else l = 0; // 不同，清空
+        if (r > 0 && node.val == node.right.val) total += r;
+        else r = 0; // 不同，清空
+        max = Math.max(max, total - 1);
+        return 1 + Math.max(l, r);
+    }
+}
