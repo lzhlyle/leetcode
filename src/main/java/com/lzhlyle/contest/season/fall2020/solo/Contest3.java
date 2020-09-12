c1a28c6ea3603d18dc18ae47ed20588c616be9b1
lzhlyle <lzhlyle@msn.cn>
2020-09-12 22:02:48 +0800

    contest: fall-2020.

--- src/main/java/com/lzhlyle/contest/season/fall2020/solo/Contest3.java
+++ src/main/java/com/lzhlyle/contest/season/fall2020/solo/Contest3.java
@@ -1,46 +1,74 @@
 package com.lzhlyle.contest.season.fall2020.solo;
 
 public class Contest3 {
-    // wa
     public int minimumOperations(String leaves) {
         char[] arr = leaves.toCharArray();
         int n = arr.length;
-        int cnt = 0;
-        cnt += arr[0] == 'r' ? 0 : 1;
-        cnt += arr[n - 1] == 'r' ? 0 : 1;
 
-        arr[0] = arr[n - 1] = 'r';
+        int[][] dp = new int[n][3];
 
-        // System.out.println(cnt);
-
-        int l = 1, r = n - 2;
-        while (l <= r && arr[l] == 'r') l++;
-        while (r >= l && arr[r] == 'r') r--;
-
-        if (l == r) {
-            if (arr[l] == 'r') return cnt + 1;
-            else return cnt;
+        dp[0][0] = arr[0] == 'r' ? 0 : 1;
+        for (int i = 1; i < n - 2; i++) {
+            dp[i][0] = (arr[i] == 'r' ? 0 : 1)
+                    + dp[i - 1][0];
         }
 
-        // System.out.println("l: " + l + ", r: " + r);
-
-        // 求最长连续 'y'
-        int rCnt = 0, yCnt = 0, len = 1, max = 0;
-        for (int i = l; i <= r; i++) {
-            if (arr[i] == 'r') rCnt++;
-            else {
-                yCnt++;
-                if (i > l && arr[i - 1] == 'y') len++;
-                else len = 1;
-                max = Math.max(max, len);
-            }
+        dp[1][1] = (arr[1] == 'y' ? 0 : 1) + dp[0][0];
+        for (int i = 2; i < n - 1; i++) {
+            dp[i][1] = (arr[i] == 'y' ? 0 : 1)
+                    + Math.min(dp[i - 1][0], dp[i - 1][1]);
         }
-        max = Math.max(max, len);
-        // System.out.println("rCnt: " + rCnt + ", yCnt: " + yCnt + ", max: " + max);
 
-        if (yCnt == 0) return cnt + 1;
-        return cnt + Math.min(rCnt, yCnt - max);
+        dp[2][2] = (arr[2] == 'r' ? 0 : 1) + dp[1][1];
+        for (int i = 3; i < n; i++) {
+            dp[i][2] = (arr[i] == 'r' ? 0 : 1)
+                    + Math.min(dp[i - 1][1], dp[i - 1][2]);
+        }
+
+        return dp[n - 1][2];
     }
+
+//    // wa
+//    // "ryyryyyrryyyyyryyyrrryyyryryyyyryyrrryryyyryrryrrrryyyrrrrryryyrrrrryyyryyryrryryyryyyyryyrryrryryy"
+//    public int minimumOperations(String leaves) {
+//        char[] arr = leaves.toCharArray();
+//        int n = arr.length;
+//        int cnt = 0;
+//        cnt += arr[0] == 'r' ? 0 : 1;
+//        cnt += arr[n - 1] == 'r' ? 0 : 1;
+//
+//        arr[0] = arr[n - 1] = 'r';
+//
+//        // System.out.println(cnt);
+//
+//        int l = 1, r = n - 2;
+//        while (l <= r && arr[l] == 'r') l++;
+//        while (r >= l && arr[r] == 'r') r--;
+//
+//        if (l == r) {
+//            if (arr[l] == 'r') return cnt + 1;
+//            else return cnt;
+//        }
+//
+//        // System.out.println("l: " + l + ", r: " + r);
+//
+//        // 求最长连续 'y'
+//        int rCnt = 0, yCnt = 0, len = 1, max = 0;
+//        for (int i = l; i <= r; i++) {
+//            if (arr[i] == 'r') rCnt++;
+//            else {
+//                yCnt++;
+//                if (i > l && arr[i - 1] == 'y') len++;
+//                else len = 1;
+//                max = Math.max(max, len);
+//            }
+//        }
+//        max = Math.max(max, len);
+//        // System.out.println("rCnt: " + rCnt + ", yCnt: " + yCnt + ", max: " + max);
+//
+//        if (yCnt == 0) return cnt + 1;
+//        return cnt + Math.min(rCnt, yCnt - max);
+//    }
 
     public static void main(String[] args) {
         Contest3 contest = new Contest3();
--- src/main/java/com/lzhlyle/contest/season/fall2020/solo/Contest4.java
+++ src/main/java/com/lzhlyle/contest/season/fall2020/solo/Contest4.java
@@ -1,28 +1,52 @@
 package com.lzhlyle.contest.season.fall2020.solo;
 
-import java.util.HashSet;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Contest4 {
-    // tle
+    private long inc, dec;
+    private int[] jump, cost;
+    private int MOD = 1000000007;
+
     public int busRapidTransit(int target, int inc, int dec, int[] jump, int[] cost) {
-        Queue<int[]> queue = new PriorityQueue<>((a, b) -> a[1] == b[1] ? Math.abs(target - a[0]) - Math.abs(target - b[0]) : a[1] - b[1]);
-        queue.add(new int[]{0, 0});
-        Set<Integer> set = new HashSet<>();
-        while (!queue.isEmpty()) {
-            int[] node = queue.remove();
-            int curr = node[0], cc = node[1];
-            if (curr == target) return cc;
-            if (set.add(curr + 1)) queue.add(new int[]{curr + 1, cc + inc});
-            if (curr - 1 > 0 && set.add(curr - 1)) queue.add(new int[]{curr - 1, cc + dec});
-            for (int i = 0; i < jump.length; i++) {
-                if (set.add(curr * jump[i])) queue.add(new int[]{curr * jump[i], cc + cost[i]});
-            }
-        }
-        return -1;
+        this.inc = inc;
+        this.dec = dec;
+        this.jump = jump;
+        this.cost = cost;
+        return (int) (dfs(target, new HashMap<>()) % MOD);
     }
+
+    private long dfs(int curr, Map<Integer, Long> map) {
+        if (curr == 0) return 0;
+        if (map.containsKey(curr)) return map.get(curr);
+
+        map.put(curr, (long) MOD); // 最多不超过 MOD，避免无限扩展
+        long res = (long) curr * inc;
+        for (int i = 0; i < jump.length; i++) {
+            res = Math.min(res, dfs(curr / jump[i], map) + cost[i] + (curr % jump[i]) * inc);
+            res = Math.min(res, dfs((curr / jump[i]) + 1, map) + cost[i] + (jump[i] - (curr % jump[i])) * dec);
+        }
+        map.put(curr, res);
+        return res;
+    }
+
+//    // tle
+//    public int busRapidTransit(int target, int inc, int dec, int[] jump, int[] cost) {
+//        Queue<int[]> queue = new PriorityQueue<>((a, b) -> a[1] == b[1] ? Math.abs(target - a[0]) - Math.abs(target - b[0]) : a[1] - b[1]);
+//        queue.add(new int[]{0, 0});
+//        Set<Integer> set = new HashSet<>();
+//        while (!queue.isEmpty()) {
+//            int[] node = queue.remove();
+//            int curr = node[0], cc = node[1];
+//            if (curr == target) return cc;
+//            if (set.add(curr + 1)) queue.add(new int[]{curr + 1, cc + inc});
+//            if (curr - 1 > 0 && set.add(curr - 1)) queue.add(new int[]{curr - 1, cc + dec});
+//            for (int i = 0; i < jump.length; i++) {
+//                if (set.add(curr * jump[i])) queue.add(new int[]{curr * jump[i], cc + cost[i]});
+//            }
+//        }
+//        return -1;
+//    }
 
     public static void main(String[] args) {
         Contest4 contest = new Contest4();
