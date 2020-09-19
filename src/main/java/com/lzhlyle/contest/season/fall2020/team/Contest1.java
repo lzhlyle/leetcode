6cb2a017f2024a3f1d191291f4f22e7d50b51d23
lzhlyle <lzhlyle@msn.cn>
2020-09-19 18:19:38 +0800

    contest: season-fall-2020.

--- src/main/java/com/lzhlyle/contest/season/fall2020/team/Contest1.java
+++ src/main/java/com/lzhlyle/contest/season/fall2020/team/Contest1.java
@@ -1,12 +1,83 @@
 package com.lzhlyle.contest.season.fall2020.team;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Contest1 {
+    private int cnt;
+    private Map<Integer, Set<Integer>> map;
+
+    public int paintingPlan(int n, int k) {
+        if (k == 0) return 1;
+        if (n > k) return 0;
+        cnt = 0;
+
+        // bitwise: long: 64 bits
+        int[][] arr = new int[n][n];
+        map = new HashMap<>();
+        dfs(0b0, 0b0, 0, 0, n, k, 0);
+        return cnt;
+    }
+
+    private void dfs(int i, int j, int ii, int jj, int n, int k, int currCnt) {
+        if (currCnt > k) return;
+        if (currCnt == k) {
+            if (!map.containsKey(i) || !map.get(i).contains(j)) {
+                System.out.printf("i: %s, j: %s, ii: %s, jj: %s, currCnt: %s%n",
+                        Integer.toBinaryString(i),
+                        Integer.toBinaryString(i),
+                        ii, jj, currCnt);
+                if (!map.containsKey(i)) map.put(i, new HashSet<>());
+                map.get(i).add(j);
+                cnt++;
+            }
+            return;
+        }
+
+        if (ii < n) {
+            // 涂
+            int nextI = i | (1 << ii);
+            dfs(nextI, j, ii + 1, jj, n, k, count(nextI, j, n));
+
+            // 不涂
+            dfs(i, j, ii + 1, jj, n, k, currCnt);
+        }
+
+        if (jj < n) {
+            // 涂
+            int nextJ = j | (1 << jj);
+            dfs(i, nextJ, ii, jj + 1, n, k, count(i, nextJ, n));
+
+            // 不涂
+            dfs(i, j, ii, jj + 1, n, k, currCnt);
+        }
+    }
+
+    private int count(int a, int b, int n) {
+        // System.out.printf("a: %s, b: %s, ", a, b);
+        int i = 0, j = 0;
+        while (a != 0) {
+            a &= a - 1;
+            i++;
+        }
+        while (b != 0) {
+            b &= b - 1;
+            j++;
+        }
+
+        // 有共 i 行、共 j 列 被涂色
+        int res = (i * n) + (j * n) - (i * j);
+        // System.out.printf("i: %s, j: %s, res: %s%n", i, j, res);
+        return res;
+    }
 
     public static void main(String[] args) {
-        System.out.println("begin");
         Contest1 contest = new Contest1();
         {
-            
+            int res = contest.paintingPlan(2, 2);
+            System.out.println(res);
         }
     }
 }
--- src/main/java/com/lzhlyle/contest/season/fall2020/team/Contest2.java
+++ src/main/java/com/lzhlyle/contest/season/fall2020/team/Contest2.java
@@ -1,11 +1,73 @@
 package com.lzhlyle.contest.season.fall2020.team;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Contest2 {
+    // wa
+    private String tt = "";
+
+    public boolean isMagic(int[] target) {
+        int n = target.length;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = i + 1;
+        tt = toStr(target);
+
+        for (int k = 1; k <= n; k++) {
+            Set<String> set = new HashSet<>();
+            set.add(toStr(arr));
+            if (dfs(arr.clone(), 0, set, target, k)) return true;
+            System.out.println();
+        }
+        return false;
+    }
+
+    private boolean dfs(int[] arr, int i, Set<String> set, int[] target, int k) {
+        if (i == arr.length) return true;
+
+        // 移动索引到从 i 开始
+        move(arr, i);
+        if (!set.add(toStr(arr))) return false;
+
+        int kk = k, j = i;
+        while (kk > 0 && j < arr.length && arr[j] == target[j]) {
+            kk--;
+            j++;
+        }
+        if (j == arr.length) return true;
+        if (kk == 0) {
+            // 取前 k 个
+            if (toStr(arr).equals(tt)) return true;
+            if (dfs(arr, i + k, set, target, k)) return true;
+        }
+        return false;
+    }
+
+    private void move(int[] arr, int i) {
+        for (int to = i, from = i + 1; from < arr.length; from += 2, to++) {
+            int temp = arr[from];
+            int j = from - 1;
+            while (j >= to) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[to] = temp;
+        }
+    }
+
+    private String toStr(int[] arr) {
+        StringBuilder builder = new StringBuilder();
+        for (int v : arr) builder.append(v);
+        System.out.println(builder.toString());
+        return builder.toString();
+    }
 
     public static void main(String[] args) {
         Contest2 contest = new Contest2();
         {
-            
+            boolean res = contest.isMagic(new int[]{2, 4, 3, 1, 5});
+            System.out.println(res);
         }
     }
 }
