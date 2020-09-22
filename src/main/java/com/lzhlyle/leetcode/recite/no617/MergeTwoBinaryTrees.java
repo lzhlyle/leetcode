34ab10f7de4f8e44a09d87d74acccfc5f76639a0
lzhlyle <lzhlyle@msn.cn>
2020-09-23 00:10:18 +0800

    recite and self: 617.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no617/MergeTwoBinaryTrees.java
@@ -0,0 +1,13 @@
+package com.lzhlyle.leetcode.recite.no617;
+
+public class MergeTwoBinaryTrees {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) return t2;
+        if (t2 == null) return t1;
+
+        TreeNode res = new TreeNode(t1.val + t2.val);
+        res.left = mergeTrees(t1.left, t2.left);
+        res.right = mergeTrees(t1.right, t2.right);
+        return res;
+    }
+}
---
+++ src/main/java/com/lzhlyle/leetcode/recite/no617/TreeNode.java
@@ -0,0 +1,11 @@
+package com.lzhlyle.leetcode.recite.no617;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
