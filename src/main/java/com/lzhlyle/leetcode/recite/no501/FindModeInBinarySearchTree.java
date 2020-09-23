ccd4325a949fe4a01d76ba9a3f3b37444db2198a
lzhlyle <lzhlyle@msn.cn>
2020-09-24 01:22:47 +0800

    recite and self: 501.

---
+++ src/main/java/com/lzhlyle/leetcode/recite/no501/FindModeInBinarySearchTree.java
@@ -0,0 +1,50 @@
+package com.lzhlyle.leetcode.recite.no501;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindModeInBinarySearchTree {
+    private int max, cnt;
+    private Integer last;
+    private List<Integer> list;
+
+    public int[] findMode(TreeNode root) {
+        if (root == null) return new int[0];
+
+        max = cnt = 1;
+        last = null;
+        list = new ArrayList<>();
+
+        dfs(root, list);
+        if (cnt == max) {
+            list.add(last);
+        } else if (cnt > max) {
+            list.clear();
+            list.add(last);
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private void dfs(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        dfs(node.left, list);
+
+        if (last == null) last = node.val;
+        else if (node.val == last) {
+            cnt++;
+        } else {
+            if (cnt == max) {
+                list.add(last);
+            } else if (cnt > max) {
+                list.clear();
+                list.add(last);
+                max = cnt;
+            }
+            last = node.val;
+            cnt = 1;
+        }
+
+        dfs(node.right, list);
+    }
+}
