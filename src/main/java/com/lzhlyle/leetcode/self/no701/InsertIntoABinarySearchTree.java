1138cf92acd8e2083b4beb2a135608fc7c7bfaa0
lzhlyle <lzhlyle@msn.cn>
2020-09-30 00:08:29 +0800

    recite and self: 701.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no701/InsertIntoABinarySearchTree.java
@@ -0,0 +1,10 @@
+package com.lzhlyle.leetcode.self.no701;
+
+public class InsertIntoABinarySearchTree {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        if (root.val < val) root.right = insertIntoBST(root.right, val);
+        else root.left = insertIntoBST(root.left, val);
+        return root;
+    }
+}
