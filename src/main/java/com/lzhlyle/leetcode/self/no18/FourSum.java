40437583281a37cacf3206a683fc8d82e794c58d
lzhlyle <lzhlyle@msn.cn>
2020-10-05 00:49:20 +0800

    recite and self: 18.

---
+++ src/main/java/com/lzhlyle/leetcode/self/no18/FourSum.java
@@ -0,0 +1,38 @@
+package com.lzhlyle.leetcode.self.no18;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FourSum {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        int len = nums.length;
+        for (int i = 0; i < len - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+            if (nums[i] + nums[len - 3] + nums[len - 2] + nums[len - 1] < target) continue;
+            for (int j = i + 1; j < len - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
+                if (nums[i] + nums[j] + nums[len - 2] + nums[len - 1] < target) continue;
+                int curr = nums[i] + nums[j];
+                int l = j + 1, r = len - 1;
+                while (l < r) {
+                    int sum = curr + nums[l] + nums[r];
+                    if (sum > target) r--;
+                    else if (sum < target) l++;
+                    else {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+                        while (l < r && nums[l] == nums[l + 1]) l++;
+                        l++;
+                        while (l < r && nums[r] == nums[r - 1]) r--;
+                        r--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
